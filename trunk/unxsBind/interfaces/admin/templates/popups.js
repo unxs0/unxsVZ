89a20532ef00571730e6b626a5f4e51fb05f3a21
Dylan <Dylan@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-05-13 17:56:19 +0000

    Replacing unxsBind with a clean version for new rpm distro system.

--- trunk/unxsBind/interfaces/admin/templates/popups.js
+++
@@ -1,8 +0,0 @@
-function open_popup(page)
-{
-	window_handle = window.open(page,'Glossary','height=600,width=500,status=yes,toolbar=no,menubar=no,location=no,scrollbars=1');
-	window_handle.focus();
-	return false;
-
-}//function open_popup(page)
-
