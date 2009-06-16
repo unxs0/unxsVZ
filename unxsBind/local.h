21663eb92b0117f0c5e3b114dad40f3b706e30ab
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-06-16 14:40:16 +0000

    unxsBind: updated .spec for new rpm release

--- unxsBind/local.h
+++ unxsBind/local.h
@@ -53,5 +53,5 @@ LEGAL
 //table related files.
 
 //For openisp staff mod only. Staff: Remember to change makefile also.
-#define RELEASE "Release 2.7/rpm-1.13"
-#define REV "2.7/rpm-1.13"
+#define RELEASE "Release 2.7/rpm-1.14"
+#define REV "2.7/rpm-1.14"
