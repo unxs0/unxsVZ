37ff78c80a230d35d1d0ba076ec4291887fc1033
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-06-19 21:13:18 +0000

    unxsMailUser: save commit

--- trunk/unxsMail/interfaces/user/myaccount.c
+++ trunk/unxsMail/interfaces/user/myaccount.c
@@ -16,7 +16,7 @@ PURPOSE
 #define STATUS_PENDING 1
 #define STATUS_PENDING_MOD 2
 
-static char cLogin[65]={""};
+static char cOldPassword[65]={""};
 static char cPasswd[65]={""};
 static char cConfirmPasswd[65]={""};
 
@@ -54,6 +54,9 @@ void ProcessMyAccountVars(pentry entries[], int x)
 			sprintf(cPasswd,"%.65s",entries[i].val);
 		else if(!strcmp(entries[i].name,"cConfirmPassword"))
 			sprintf(cConfirmPasswd,"%.65s",entries[i].val);
+		else if(!strcmp(entries[i].name,"cOldPassword"))
+			sprintf(cOldPassword,"%.65s",entries[i].val);
+
 	}
 
 }//void ProcessMyAccountVars(pentry entries[], int x)
@@ -80,38 +83,13 @@ void MyAccountCommands(pentry entries[], int x)
 		ProcessMyAccountVars(entries,x);
 		if(!strcmp(gcFunction,"Update your password"))
 		{
+			if(!ValidateInput())
+				htmlMyAccount();
 		}
 		htmlMyAccount();
 	}
 
 }//void MyAccountCommands(pentry entries[], int x)
-
-
-void htmlPasswordReset(unsigned uStep)
-{
-	htmlHeader("unxsMail User Interface","Header");
-	sprintf(gcQuery,"MyAccountPwdReset.%u",uStep);
-	htmlMyAccountPage("unxsMail User Interface",gcQuery);
-	htmlFooter("Footer");
-	
-}//void htmlPasswordReset(void)
-
-
-void htmlCreateAccount(void)
-{
-	htmlHeader("unxsMail User Interface","Header");
-	htmlMyAccountPage("unxsMail User Interface","MyAccountCreate");
-	htmlFooter("Footer");
-	
-}//void htmlCreateAccount(void)
-
-
-void htmlCmdOutput(void)
-{
-	 htmlHeader("unxsMail User Interface","Header");
-	 htmlMyAccountPage("unxsMail User Interface","MyAccountPwdMsg");
-	 htmlFooter("Footer");
-}//void htmlCmdOutput(void)
 
 
 void htmlMyAccount(void)
@@ -140,7 +118,7 @@ void htmlMyAccountPage(char *cTitle, char *cTemplateName)
 			template.cpValue[0]=cTitle;
 			
 			template.cpName[1]="cCGI";
-			template.cpValue[1]="index.cgi";
+			template.cpValue[1]="unxsMailUser.cgi";
 			
 			template.cpName[2]="gcLogin";
 			template.cpValue[2]=gcUser;
@@ -157,7 +135,10 @@ void htmlMyAccountPage(char *cTitle, char *cTemplateName)
 			template.cpName[6]="gcHost";
 			template.cpValue[6]=gcHost;
 
-			template.cpName[7]="";
+			template.cpName[7]="gcMessage";
+			template.cpValue[7]=gcMessage;
+
+			template.cpName[8]="";
 
 			printf("\n<!-- Start htmlMyAccountPage(%s) -->\n",cTemplateName); 
 			Template(field[0], &template, stdout);
@@ -176,24 +157,29 @@ void htmlMyAccountPage(char *cTitle, char *cTemplateName)
 
 unsigned ValidateInput(void)
 {
+	if(!cOldPassword[0])
+	{
+		gcMessage="<blink>Error: </blink>You must enter your current password";
+		return(0);
+	}
 	if(!cPasswd[0])
 	{
-		gcMessage="<blink>You must enter a password</blink>";
+		gcMessage="<blink>Error: </blink>You must enter a password";
 		return(0);
 	}
 	if(!cConfirmPasswd[0])
 	{
-		gcMessage="<blink>You must confirm your password</blink>";
+		gcMessage="<blink>Error: </blink>You must confirm your password";
 		return(0);
 	}
 	if(strcmp(cPasswd,cConfirmPasswd))
 	{
-		gcMessage="<blink>The entered passwords do not match</blink>";
+		gcMessage="<blink>Error: </blink>The entered passwords do not match";
 		return(0);
 	}
 	if(strlen(cPasswd)<5)
 	{
-		gcMessage="<blink>Password must have 5 characters as minimum length</blink>";
+		gcMessage="<blink>Error: </blink>Password must have 5 characters as minimum length";
 		return(0);
 	}
 		
@@ -205,10 +191,10 @@ unsigned ValidateInput(void)
 
 unsigned Update_tUser(char *cPasswd)
 {
-	sprintf(gcQuery,"UPDATE tUser SET cPasswd='%s',uStatus=%u,uModBy=1,uModDate=UNIX_TIMESTAMP(NOW()) WHERE cLogin='%s'",
+	sprintf(gcQuery,"UPDATE tUser SET cPasswd='%s',uStatus=%u,uModBy=1,uModDate=UNIX_TIMESTAMP(NOW()) WHERE uUser=%u'",
 			cPasswd
 			,STATUS_PENDING_MOD
-			,cLogin);
+			,guLoginClient);
 	mysql_query(&gMysql,gcQuery);
 
 	if(mysql_errno(&gMysql))
