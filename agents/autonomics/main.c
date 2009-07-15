955ebe99d6a1c5a6e08eea76dcf8a26230a76193
Gary <ggw@unixservice.com>
2009-07-15 21:55:00 +0000

    unxsVZ initial skeleton for standalone node autonomics daemon.

--- agents/autonomics/main.c
+++ agents/autonomics/main.c
@@ -4,33 +4,28 @@ FILE
 	$Id$
 	/usr/sbin/unxsvz-autonomics
 AUTHOR
-	(C) 2006-2009, Gary Wallis for Unixservice USA
+	(C) 2009, Gary Wallis for Unixservice USA
 PURPOSE
+	Calculate every 10 seconds autonomic rules.
+	If a rule is triggered take configured action.
+	This file: Start, stop handle signals of autonomics daemon.
 NOTES
-TODO
 */
 
-#include "../../mysqlrad.h"
-#include <ctype.h>
+#include "autonomics.h"
 
-//#define DBIP "localhost"
-#define ERRLOG "/var/log/unxsvz-autonomics.log"
-
-void daemonize(void);
-void sighandlerLeave(int iSig);
-void logfileLine(char *cLogline);
-
-//global data
+//global data storage
 FILE *gEfp;
 MYSQL gMysql;
 char gcLine[256];
+unsigned guDryrun=0;
+char gcHostname[100];
+char gcQuery[1024];
+unsigned guDatacenter=0;
+unsigned guNode=0;
 
 int main(int iArgc, char *cArgv[])
 {
-//	MYSQL_RES *res;
-//	MYSQL_ROW field;
-	char cHostname[100];
-
 	if(iArgc==2 && !strncmp(cArgv[1],"--fg",4))
 	{
 		printf("Running %s in foreground\n",cArgv[0]);
@@ -38,39 +33,60 @@ int main(int iArgc, char *cArgv[])
 	}
 	else if(iArgc==2 && !strncmp(cArgv[1],"--help",6))
 	{
-		printf("%s help\n\n--help\tthis page\n--fg\trun program in foreground mode\n",
+		printf("%s help\n\n--help\t\tthis page\n--fg\t\trun program in foreground mode\n"
+				"--dryrun\treport recommended autonomics in log but take no action\n",
 				cArgv[0]);
 		return(0);
 	}
-	else if(iArgc>=2)
+	else if(iArgc>=2 && strncmp(cArgv[1],"--dryrun",8))
 	{
-		printf("Usage: %s [--help | --fg]\n",cArgv[0]);
+		printf("Usage: %s [--help | --fg | --dryrun]\n",cArgv[0]);
 		return(0);
 	}
 	else if(1)
 	{
+		if(iArgc==2 && !strncmp(cArgv[1],"--dryrun",8))
+			guDryrun=1;
 		daemonize();
 		if((gEfp=fopen(ERRLOG,"a"))==NULL)
 		{
 			fprintf(stderr,"Could not open error log!\n");
-			return(3);
+			return(300);
         	}
 		//Set signal handler
 		(void) signal(SIGINT,sighandlerLeave);
+		(void) signal(SIGHUP,sighandlerLeave);
+		(void) signal(SIGTERM,sighandlerLeave);
 	}
 
         mysql_init(&gMysql);
+        if(!mysql_real_connect(&gMysql,DBIP0,DBLOGIN,DBPASSWD,DBNAME,0,NULL,0))
+        {
+		if(!mysql_real_connect(&gMysql,DBIP1,DBLOGIN,DBPASSWD,DBNAME,0,NULL,0))
+		{
+               		logfileLine("database server unavailable\n");
+			fclose(gEfp);
+			mysql_close(&gMysql);
+			return(100);
+		}
+	}
 
-	gethostname(cHostname,99);
-	sprintf(gcLine,"%s started and mysql_init() ran ok. cHostname=%s",cArgv[0],cHostname);
+	gethostname(gcHostname,99);
+	sprintf(gcLine,"%s started. gcHostname=%s, guDryrun=%u",
+			cArgv[0],gcHostname,guDryrun);
 	logfileLine(gcLine);
 
 	while(1)
 	{
-		;
+		sleep(7);
+		if(DatacenterAutonomics())
+			break;
+		if(NodeAutonomics())
+			break;
+		if(ContainerAutonomics())
+			break;
 	}//while(1)
 
-//Return_Point:
 	logfileLine("abnormal ending");
 	fclose(gEfp);
 	mysql_close(&gMysql);
