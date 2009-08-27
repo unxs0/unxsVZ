c2ec4c987a906acbcc1287f4d5f75d310f486585
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-08-27 21:17:24 +0000

    unxsVZ: updated .spec file for new rpm release

--- local.h
+++ local.h
@@ -46,6 +46,6 @@ LEGAL
 
 //End-users: Do not change these!
 //Developers: See makefile also when changing these
-#define RELEASE "1.21/rpm-2.1"
-#define REV "1.21/rpm-2.1"
+#define RELEASE "1.21/rpm-2.2"
+#define REV "1.21/rpm-2.2"
 
