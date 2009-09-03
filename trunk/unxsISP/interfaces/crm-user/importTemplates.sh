a7f999bc4318765dde882c23ed38f760504fe8c7
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-09-03 20:39:05 +0000

    ispHelp: save commit

---
+++ trunk/unxsISP/interfaces/crm-user/importTemplates.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+#
+#FILE
+#	$Id: importTemplates.sh 85 2009-05-26 15:30:04Z Hugo $
+#PURPOSE
+#	Import ispAdmin.cgi templates in one fell swoop.
+#	Very useful when moving application to new server.
+#	Or during development work with vi editing of templates.
+#AUTHOR
+#	Hugo Urquiza for Unixservice (C) 2007-2009
+#
+#
+for i in `find ./templates/* -type f`; do 
+	/var/www/unxs/cgi-bin/unxsISP.cgi ImportTemplateFile  `echo $i | cut -f 3 -d /`  ./$i help-english;
+done
