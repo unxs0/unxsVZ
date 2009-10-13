aeed13fe9d55dbee321b7b8516980cc6fee291b1
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-10-13 22:55:53 +0000

    unxsBind.php: save commit

--- unxsBind/interfaces/php/simpleapp.php
+++ unxsBind/interfaces/php/simpleapp.php
@@ -1,6 +1,41 @@
 <?php
 require_once('unxsapi.php');
 
+if(count($_GET))
+{
+	ConnectDb();
+
+	$cZone=$_GET['cZone'];
+	$Zone=new unxsBindZone();
+	$Zone->uOwner=5;
+	$Zone->uCreatedBy=6;
+	$Zone->cZone=$cZone;
+
+	echo("Zone name: $cZone<br>");
+	echo("Zone RRs:<br>");
+	
+	$RRs=$Zone->GetRRs();
+	?>
+	<table>
+	<tr><th>Name</th><th>TTL</th><th>Type</th><th>Param 1</th><th>Param 2</th><th>Param 3</th><th>Param 4</th><th>Comment</th></tr>
+	<?
+	foreach ($RRs as $RR)
+	{
+		echo("<tr>");
+		echo("<td>".$RR->GetProperty("Name")."</td>");
+		echo("<td>".$RR->GetProperty("TTL")."</td>");
+		echo("<td>".$RR->GetProperty("Type")."</td>");
+		echo("<td>".$RR->GetProperty("Param 1")."</td>");
+		echo("<td>".$RR->GetProperty("Param 2")."</td>");
+		echo("<td>".$RR->GetProperty("Param 3")."</td>");
+		echo("<td>".$RR->GetProperty("Param 4")."</td>");
+		echo("<td>".$RR->GetProperty("Comment")."</td>");
+		echo("</tr>");
+	}
+	echo("</table>");
+	return;
+}
+
 if(!count($_POST))
 {
 	$cMessage="&nbsp;";
