2416d8a9fa3297387242430451d514d8307755d8
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-10-22 18:43:25 +0000

    unxsApache: Updated ExtConnectDb() default database and login values

--- unxsApache/apache.c
+++ unxsApache/apache.c
@@ -1946,9 +1946,9 @@ void ExtConnectDb(unsigned uHtml)
 {
 	char cDbIp[256]={""};
 	char *cEffectiveDbIp=NULL;
-	char cDbName[256]={"mysqlisp2"};
+	char cDbName[256]={"unxsisp"};
 	char cDbPwd[256]={"wsxedc"};
-	char cDbLogin[256]={"mysqlisp2"};
+	char cDbLogin[256]={"unxsisp"};
 
 	GetConfiguration("cExtJobQueueDbIp",cDbIp,0,1);
 	GetConfiguration("cExtJobQueueDbName",cDbName,0,1);
