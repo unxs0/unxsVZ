35ad18c28e478be519f3198b63106475f28e6332
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-11-07 13:47:08 +0000

    ispClient: save commit

--- trunk/unxsISP/interfaces/customer/payment.c
+++ trunk/unxsISP/interfaces/customer/payment.c
@@ -13,21 +13,27 @@ AUTHOR
 unsigned https(char const *cHost,unsigned uPort,char const *cURL,char const *cPost,char *cResult);
 
 
-char *SubmitRequest(unsigned uInvoice)
+unsigned SubmitRequest(unsigned uInvoice)
 {
+	//Return 1 on approved transaction, 0 on failed
+
 	MYSQL_RES *res;
 	MYSQL_ROW field;
 	char cPost[4096]={""};
-	char *cHost="";
-	char *cURL="";
+	//Define for your payment gateway, response parsing may of course change 
+	//depending on the payment gateway processor
+	char *cHost="www.beanstream.com";
+	char *cURL="/scripts/process_transaction.asp";
 	unsigned uPort=443;
 	char cResult[2048]={""};
-	static char cRet[256]={""};
 	unsigned uMerchantID=0;
 	char cMerchantID[100]={""};
 
 	GetConfiguration("cMerchantID",cMerchantID);
 	sscanf(cMerchantID,"%u",&uMerchantID);
+	
+	if(!uMerchantID)
+		htmlPlainTextError("Fatal: No merchant ID configured, can't continue");
 
 	sprintf(gcQuery,"SELECT uOwner,cFirstName,cLastName,cEmail,cAddr1,cAddr2,cCity,cZip,cTelephone,cFax,mTotal,"
 			"cCardNumber,uExpMonth,uExpYear,cCardName,cState  "
@@ -63,18 +69,35 @@ char *SubmitRequest(unsigned uInvoice)
 
 		if(!https(cHost,uPort,cURL,cPost,cResult))
 		{
-			//char cBuffer[1024];
-			//Debug only
-			//FILE *fp;
-			//if((fp=fopen("/tmp/cLPResult.txt","w")))
-			//{
-			//	fprintf(fp,"%.1023s\n\n********************%.10239s\n",cPost,cResult);
-			//}
-			//fclose(fp);
-			return(cRet);
+		/*
+		Sample aproved response:
+		trnApproved=1&trnId=10003067&messageId=1&messageText=Approved&trnOrderNumber=E40089&aut
+		hCode=TEST&errorType=N&errorFields=&responseType=T&trnAmount=10%2E00&trnDate=1%2F17%2F
+		2008+11%3A36%3A34+AM&avsProcessed=0&avsId=0&avsResult=0&avsAddrMatch=0&avsPostalMatch=
+		0&avsMessage=Address+Verification+not+performed+for+this+transaction%2E&rspCodeCav=0&rspCavR
+		esult=0&rspCodeCredit1=0&rspCodeCredit2=0&rspCodeCredit3=0&rspCodeCredit4=0&rspCodeAddr1=0&r
+		spCodeAddr2=0&rspCodeAddr3=0&rspCodeAddr4=0&rspCodeDob=0&rspCustomerDec=&trnType=P&pay
+		mentMethod=CC&ref1=&ref2=&ref3=&ref4=&ref5=
+		
+		
+		Sample declined response:
+		trnApproved=0&trnId=10003068&messageId=16&messageText=Duplicate+Transaction+%2D+This+tran
+		saction+has+already+been+approved&trnOrderNumber=E40089&authCode=&errorType=N&errorFields=
+		&responseType=T&trnAmount=10%2E00&trnDate=1%2F17%2F2008+11%3A38%3A10+AM&avsProcesse
+		d=0&avsId=0&avsResult=0&avsAddrMatch=0&avsPostalMatch=0&avsMessage=Address+Verification+not
+		+performed+for+this+transaction%2E&rspCodeCav=0&rspCavResult=0&rspCodeCredit1=0&rspCodeCredi
+		t2=0&rspCodeCredit3=0&rspCodeCredit4=0&rspCodeAddr1=0&rspCodeAddr2=0&rspCodeAddr3=0&rspCo
+		deAddr4=0&rspCodeDob=0&rspCustomerDec=&trnType=P&paymentMethod=CC&ref1=&ref2=&ref3=&ref
+		4=&ref5=
+		*/
+			if(strstr(cResult,"trnApproved=1"))
+				return(1);
+			else
+				return(0);
 		}
 	}
 	
-	return("");
-}
+	return(0);
+
+}//unsigned SubmitRequest(unsigned uInvoice)
 
