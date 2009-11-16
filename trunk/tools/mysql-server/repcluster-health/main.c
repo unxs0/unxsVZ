42a28ee917acf9df636aa415d97d8b4256e8a59b
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-11-16 20:03:24 +0000

    repcluster-health: save commit

--- trunk/tools/mysql-server/repcluster-health/main.c
+++ trunk/tools/mysql-server/repcluster-health/main.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
-#include <mysql.h>
+#include <mysql/mysql.h>
 #include <stdlib.h>
+
+void ConnectMySQL(MYSQL *aMySQL,char *cIP,char *cLogin,char *cPwd,char *cDb);
 
 int main(void)
 {
-	char *cServerIps={"70.38.123.40","70.38.123.41","70.38.123.42",""};
+	char *cServerIps[5]={"70.38.123.40","70.38.123.41","70.38.123.42",""};
 	char *cDbLogin="test";
 	char *cDbPwd="wsx3edc";
 	char *cDbName="test";
-	MySQL MySQLVectors[5]; //Up to 6 MySQL servers can be define in the cServerIps array
+	char cQuery[1024]={""};
 
-	ConnectMySQL(&MySQLVectors,cServerIps,cDbLogin,cDbPwd,cDbName);
+	MYSQL MySQLVectors[5]; //Up to 6 MySQL servers can be define in the cServerIps array
+	int i=0;
+	int j;
+	int x=0;
+
+	while(1)
+	{
+		ConnectMySQL(&MySQLVectors[i],cServerIps[i],cDbLogin,cDbPwd,cDbName);
+		if(!cServerIps[i+1]) break;
+		i++;
+	}
+	printf("Connected to %i MySQL servers OK\n",i);
+	
+	for(j=0;j<i;j++)
+	{
+		sprintf(cQuery,"CREATE TABLE delme%i (cData VARCHAR 16)",j);
+		mysql_query(&MySQLVectors[j],cQuery);
+		if(mysql_errno(&MySQLVectors[j]))
+		{
+			printf("Fatal: CREATE TABLE failed for server #%i\n",j);
+			printf("Error was: %s\n",mysql_error(&MySQLVectors[j]));
+			exit(EXIT_FAILURE);
+		}
+	}
+	for(j=0;j<i;j++)
+		mysql_close(&MySQLVectors[j]);
+	
+	printf("Disconnected from %i MySQL servers OK\n",j);
+
+	return(EXIT_SUCCESS);
 
 }//main
 
 
-void ConnectMySQL(MYSQL *aMySQL[],char *cIPs[],char *cLogin,char *cPwd,char *cDb)
+void ConnectMySQL(MYSQL *aMySQL,char *cIP,char *cLogin,char *cPwd,char *cDb)
 {
-	int i=0;
-	while(1)
+	mysql_init(aMySQL);
+	if(mysql_real_connect(aMySQL,cIP,cLogin,cPwd,cDb,0,NULL,0))
 	{
-		mysql_init(aMySQL[i]);
-		if(mysql_real_connect(aMySQL[i],cIPs[i],cLogin,cPwd,cDb,NULL,0))
-		{
-			printf("Fatal: failed to connect MySQL vector %i.\n");
-			printf("IP address    : %s\n",cIPs[i]);
-			printf("Error message : %s\n",mysql_error(aMySQL[i]));
-			exit(EXIT_FAILURE);
-		}
+		printf("Fatal: failed to connect MySQL vector");
+		printf("IP address    : %s\n",cIP);
+		printf("Error message : %s\n",mysql_error(aMySQL));
+		exit(EXIT_FAILURE);
 	}
 
-Ã//void ConnectMySQ
+}//void ConnectMySQL(MYSQL *aMySQL[],char *cIPs[],char *cLogin,char *cPwd,char *cDb)
 
