8cd180caf56bbb72fef070b9fa77ad18b44cee37
Hugo <Hugo@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-11-16 20:36:17 +0000

    repcluster-health: save commit

--- trunk/tools/mysql-server/repcluster-health/main.c
+++ trunk/tools/mysql-server/repcluster-health/main.c
@@ -8,26 +8,26 @@ int main(void)
 {
 	char *cServerIps[5]={"70.38.123.40","70.38.123.41","70.38.123.42",""};
 	char *cDbLogin="test";
-	char *cDbPwd="wsx3edc";
+	char *cDbPwd="wsxedc";
 	char *cDbName="test";
 	char cQuery[1024]={""};
 
-	MYSQL MySQLVectors[5]; //Up to 6 MySQL servers can be define in the cServerIps array
+	MYSQL MySQLVectors[5]; //Up to 6 MySQL servers can be defined in the cServerIps array
 	int i=0;
 	int j;
-	int x=0;
+	int x;
 
 	while(1)
 	{
 		ConnectMySQL(&MySQLVectors[i],cServerIps[i],cDbLogin,cDbPwd,cDbName);
-		if(!cServerIps[i+1]) break;
 		i++;
+		if(!cServerIps[i][0]) break;
 	}
 	printf("Connected to %i MySQL servers OK\n",i);
 	
 	for(j=0;j<i;j++)
 	{
-		sprintf(cQuery,"CREATE TABLE delme%i (cData VARCHAR 16)",j);
+		sprintf(cQuery,"CREATE TABLE delme%i (cData VARCHAR(16) NOT NULL DEFAULT '')",j);
 		mysql_query(&MySQLVectors[j],cQuery);
 		if(mysql_errno(&MySQLVectors[j]))
 		{
@@ -36,6 +36,25 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 	}
+
+	for(j=0;j<i;j++)
+	{
+		for(x;<i;x++)
+		{
+			MYSQL_RES *res;
+			MYSQL_ROW field;
+
+			sprintf(gcQuery,"CHECKSUM TABLE delme%i",x);
+			mysql_query(&MySQLVectors[j],cQuery);
+			if(mysql_errno(&MySQLVectors[j]))
+			{
+				printf("Fatal: CHECKSUM TABLE failed for server #%i\n",j);
+				printf("Error was: %s\n",mysql_error(&MySQLVectors[j]));
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+
 	for(j=0;j<i;j++)
 		mysql_close(&MySQLVectors[j]);
 	
@@ -49,13 +68,13 @@ int main(void)
 void ConnectMySQL(MYSQL *aMySQL,char *cIP,char *cLogin,char *cPwd,char *cDb)
 {
 	mysql_init(aMySQL);
-	if(mysql_real_connect(aMySQL,cIP,cLogin,cPwd,cDb,0,NULL,0))
+	if(!mysql_real_connect(aMySQL,cIP,cLogin,cPwd,cDb,0,NULL,0))
 	{
 		printf("Fatal: failed to connect MySQL vector");
 		printf("IP address    : %s\n",cIP);
 		printf("Error message : %s\n",mysql_error(aMySQL));
 		exit(EXIT_FAILURE);
 	}
-
+	
 }//void ConnectMySQL(MYSQL *aMySQL[],char *cIPs[],char *cLogin,char *cPwd,char *cDb)
 
