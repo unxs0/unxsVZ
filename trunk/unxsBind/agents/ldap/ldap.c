20fae4071b8bfd1929db49218cb254a1688eb5e5
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2009-12-30 21:37:29 +0000

    Initial ldap.c stub agent

---
+++ trunk/unxsBind/agents/ldap/ldap.c
@@ -0,0 +1,17 @@
+/*
+FILE
+	ldap.c
+	$Id$
+PURPOSE
+	Get authentication and authorization data from an LDAP server
+	using libldap.
+AUTHOR
+	(C) Gary Wallis for Unixservice, LLC. 2009-2010.
+NOTES
+	Starting out very simple.
+*/
+
+int main(int iArgc, char *cArgv[])
+{
+	return(0);
+}//main()
