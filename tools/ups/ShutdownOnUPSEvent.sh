0bec3f681a627ffe9256b66797410e705d17ad20
Gary <ggw@unixservice.com>
2010-01-22 12:59:17 +0000

    unxsVZ tools/ups/ShutdownOnUPSEvent.sh improvements.

--- tools/ups/ShutdownOnUPSEvent.sh
+++ tools/ups/ShutdownOnUPSEvent.sh
@@ -7,32 +7,53 @@
 #	uBatteryCapacity initiate shutdown sequence.
 #AUTHOR
 #	Gary Wallis and Hugo Urquiza for Unixservice, LLC. (C) 2009-2010
+#NOTES
+#	This is experimental alpha proof of concept code. Use at your
+#	own peril. Patch and/or supply your own version and contribute it 
+#	to this project.
 
 fLog() { echo "`date +%b' '%d' '%T` $0[$$]: $@"; }
 
 #Set these for your SNMP enabled UPS
 cHost="ups0.yourisp.com";
-cSnmpGet="/usr/bin/snmpget -v 1 -c publiccommstr ups0.yourisp.com";
+cSnmpGet="/usr/bin/snmpget -v 1 -c publiccommstr $cHost";
 
 cBatCapMIB="mib-2.33.1.2.4.0";
 cACInMIB="mib-2.33.1.3.3.1.3.1";
 
-#ping test to ups
-/bin/ping -c 3 $cHost > /dev/null 2>&1;
+#fast abort if host does not respond
+/bin/ping -c 1 $cHost > /dev/null 2>&1;
 if [ "$?" != "0" ]; then
 	fLog "$cHost seems down";
 	exit 1;
 fi
 
-uBatteryCapacity=`$cSnmpGet $cBatCapMIB | cut -f 4 -d : | cut -f 2 -d " "`;
+#fast abort if snmpget does not work.
+$cSnmpGet $cBatCapMIB > /dev/null 2>&1;
 if [ $? != 0 ];then
 	fLog "$cBatCapMIB error";
+	exit 1;
+fi
+
+uBatteryCapacity=`$cSnmpGet $cBatCapMIB | cut -f 4 -d : | cut -f 2 -d " "`;
+if [ $? != 0 ];then
+	fLog "$cBatCapMIB uBatteryCapacity error";
+	exit 1;
+fi
+
+if [ -z "$uBatteryCapacity" ];then
+	fLog "$cBatCapMIB uBatteryCapacity null error";
 	exit 1;
 fi
 
 uACLineVoltage=`$cSnmpGet $cACInMIB | cut -f 4 -d : | cut -f 2 -d " "`;
 if [ $? != 0 ];then
 	fLog "$cACInMIB error";
+	exit 1;
+fi
+
+if [ -z "$uACLineVoltage" ];then
+	fLog "$cACInMIB null error";
 	exit 1;
 fi
 
@@ -64,6 +85,12 @@ else
 	sleep $uSleep;
 
 	uBatteryCapacity2=`$cSnmpGet $cBatCapMIB | cut -f 4 -d : | cut -f 2 -d " "`;
+	if [ -z "$uBatteryCapacity2" ];then
+		fLog "$cBatCapMIB uBatteryCapacity2 null error";
+		fLog "shutdown aborted";
+		exit 1;
+	fi
+
 	if (( $uBatteryCapacity2 >= $uBatteryCapacity ));then
 		#debug only
 		fLog "uBatteryCapacity2 $uBatteryCapacity2 is larger or equal to $uBatteryCapacity";
