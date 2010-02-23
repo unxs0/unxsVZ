255376371d304902567f5a701c29181c2bf79948
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2010-02-23 00:16:43 +0000

    boot-cluster/boot-slave.sh minor edit of var name and further testing passed.

--- trunk/tools/mysql-server/boot-cluster/boot-slave.sh
+++ trunk/tools/mysql-server/boot-cluster/boot-slave.sh
@@ -11,7 +11,7 @@
 #	Warning mail not implemented yet.
 
 #Configuration section
-cTimeoutSecs="300";
+iTimeoutSecs="300";
 cWarnEmail="supportgrp@unixservice.com";
 cSSHPort="22"
 #end
@@ -24,26 +24,26 @@ if [ "$1" == "" ] || [ "$2" == "" ];then
 	exit 0;
 fi
 
-#wait for master to come up. If does not come up after cTimeoutSecs send warning email, exit error.
+#wait for master to come up. If does not come up after iTimeoutSecs send warning email, exit error.
 iCounter=0;
 ping -c 1 $2 > /dev/null 2>&1;
-while [ $? != 0 ] && [ $iCounter -lt $cTimeoutSecs ];do
+while [ $? != 0 ] && [ $iCounter -lt $iTimeoutSecs ];do
 	let iCounter=iCounter+1;
 	ping -c 1 $2 > /dev/null 2>&1;
 done
-if [ $iCounter -eq "$cTimeoutSecs" ];then
+if [ $iCounter -eq "$iTimeoutSecs" ];then
 	fLog "ping master timeout $iCounter";
 	exit 1;
 fi
 
 iCounter=0;
 /usr/bin/ssh -p $cSSHPort $2 ps -ef | grep mysqld  > /dev/null 2>&1;
-while [ $? != 0 ] && [ $iCounter -lt $cTimeoutSecs ];do
+while [ $? != 0 ] && [ $iCounter -lt $iTimeoutSecs ];do
 	let iCounter=iCounter+1;
 	/usr/bin/ssh -p $cSSHPort $2 ps -ef | grep mysqld  > /dev/null 2>&1;
 	sleep 1;
 done
-if [ $iCounter -eq "$cTimeoutSecs" ];then
+if [ $iCounter -eq "$iTimeoutSecs" ];then
 	fLog "remote mysqld timeout";
 	exit 2;
 fi
@@ -74,7 +74,7 @@ if [ $? == 0 ]  && [ "$cBinLog" != "" ] && [ "$cPosition" != "" ];then
 		exit 6;
 	fi
 
-	#check status. if slave did not catch up in max cTimeoutSecs send warning email, exit error.
+	#check status. if slave did not catch up in max iTimeoutSecs send warning email, exit error.
 	eval `echo "SHOW SLAVE STATUS\G" | mysql -p$1 | grep Seconds_Behind_Master | awk '{printf"iSecondsBehindMaster=%s\n",$2}'`;
 	if [ $? != 0 ];then
 		fLog "show local slave status failed";
@@ -82,7 +82,7 @@ if [ $? == 0 ]  && [ "$cBinLog" != "" ] && [ "$cPosition" != "" ];then
 	fi
 	#echo $iSecondsBehindMaster;
 	iCounter=0;
-	while [ $iCounter -lt $cTimeoutSecs ] && [ $iSecondsBehindMaster -gt "0" ];do
+	while [ $iCounter -lt $iTimeoutSecs ] && [ $iSecondsBehindMaster -gt "0" ];do
 		let iCounter=iCounter+1;
 		eval `echo "SHOW SLAVE STATUS\G" | mysql -p$1 | grep Seconds_Behind_Master | awk '{printf"iSecondsBehindMaster=%s\n",$2}'`;
 		if [ $? != 0 ];then
@@ -92,7 +92,7 @@ if [ $? == 0 ]  && [ "$cBinLog" != "" ] && [ "$cPosition" != "" ];then
 		#double the timeout for long catch-up
 		sleep 2;
 	done
-	if [ $iCounter -eq "$cTimeoutSecs" ];then
+	if [ $iCounter -eq "$iTimeoutSecs" ];then
 		fLog "local slave catch-up timeout";
 		exit 9;
 	fi
