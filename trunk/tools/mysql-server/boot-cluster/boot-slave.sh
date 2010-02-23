f8c003bd4b664c4df58e296da0b14cc9ad194804
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2010-02-23 00:02:25 +0000

    Initial working boot-cluster/boot-slave.sh script commited.

--- trunk/tools/mysql-server/boot-cluster/boot-slave.sh
+++ trunk/tools/mysql-server/boot-cluster/boot-slave.sh
@@ -4,30 +4,103 @@
 #	boot-cluster.sh
 #	$Id$
 #LEGAL
-#	Copyright by Unixservice, LLC. 2010.
+#	Copyright Gary Wallis for Unixservice, LLC. 2010.
 #	GPLv2 license applies.
 #NOTES
-#	Assume fast internal LAN cluster interconnect with passwordless ssh set up.
+#	Requires fast internal LAN cluster interconnect with passwordless ssh set up.
+#	Warning mail not implemented yet.
 
 #Configuration section
-cMasterIP="192.168.22.128";
-cTimeoutSecs="600";
+cTimeoutSecs="300";
 cWarnEmail="supportgrp@unixservice.com";
+cSSHPort="22"
 #end
 
 #our standard bash logging function.
+fLog() { echo "`date +%b' '%d' '%T` $0[$$]: $@"; }
+
+if [ "$1" == "" ] || [ "$2" == "" ];then
+	echo "usage: $0 <mysql password> <master ip>";
+	exit 0;
+fi
 
 #wait for master to come up. If does not come up after cTimeoutSecs send warning email, exit error.
+iCounter=0;
+ping -c 1 $2 > /dev/null 2>&1;
+while [ $? != 0 ] && [ $iCounter -lt $cTimeoutSecs ];do
+	let iCounter=iCounter+1;
+	ping -c 1 $2 > /dev/null 2>&1;
+done
+if [ $iCounter -eq "$cTimeoutSecs" ];then
+	fLog "ping master timeout $iCounter";
+	exit 1;
+fi
 
-#master is up, get master status.
+iCounter=0;
+/usr/bin/ssh -p $cSSHPort $2 ps -ef | grep mysqld  > /dev/null 2>&1;
+while [ $? != 0 ] && [ $iCounter -lt $cTimeoutSecs ];do
+	let iCounter=iCounter+1;
+	/usr/bin/ssh -p $cSSHPort $2 ps -ef | grep mysqld  > /dev/null 2>&1;
+	sleep 1;
+done
+if [ $iCounter -eq "$cTimeoutSecs" ];then
+	fLog "remote mysqld timeout";
+	exit 2;
+fi
 
-#parse master status.
 
-#make sure slave mode is off.
+#master is up, get master status and parse master status.
+eval `/usr/bin/ssh -p $cSSHPort $2 "echo 'SHOW MASTER STATUS' | mysql -p$1 | grep mysql_binary_log" | awk '{printf"cBinLog=%s\ncPosition=%s\n",$1,$2}'`;
+if [ $? == 0 ]  && [ "$cBinLog" != "" ] && [ "$cPosition" != "" ];then
 
-#SQL "change master to" parsed data on local slave MySQL server.
+	#make sure slave mode is off.
+	echo "STOP SLAVE" | mysql -p$1 > /dev/null 2>&1;
+	if [ $? != 0 ];then
+		fLog "local slave stop failed";
+		exit 4;
+	fi
+
+	#SQL "change master to" parsed data on local slave MySQL server.
+	echo "CHANGE MASTER TO MASTER_LOG_FILE='$cBinLog',MASTER_LOG_POS=$cPosition" | mysql -p$1 > /dev/null 2>&1;
+	if [ $? != 0 ];then
+		fLog "local change master failed";
+		exit 5;
+	fi
+
+	#start slave
+	echo "START SLAVE" | mysql -p$1 > /dev/null 2>&1;
+	if [ $? != 0 ];then
+		fLog "local slave stop failed";
+		exit 6;
+	fi
+
+	#check status. if slave did not catch up in max cTimeoutSecs send warning email, exit error.
+	eval `echo "SHOW SLAVE STATUS\G" | mysql -p$1 | grep Seconds_Behind_Master | awk '{printf"iSecondsBehindMaster=%s\n",$2}'`;
+	if [ $? != 0 ];then
+		fLog "show local slave status failed";
+		exit 7;
+	fi
+	#echo $iSecondsBehindMaster;
+	iCounter=0;
+	while [ $iCounter -lt $cTimeoutSecs ] && [ $iSecondsBehindMaster -gt "0" ];do
+		let iCounter=iCounter+1;
+		eval `echo "SHOW SLAVE STATUS\G" | mysql -p$1 | grep Seconds_Behind_Master | awk '{printf"iSecondsBehindMaster=%s\n",$2}'`;
+		if [ $? != 0 ];then
+			fLog "show local slave status failed";
+			exit 8;
+		fi
+		#double the timeout for long catch-up
+		sleep 2;
+	done
+	if [ $iCounter -eq "$cTimeoutSecs" ];then
+		fLog "local slave catch-up timeout";
+		exit 9;
+	fi
 
 
-#start slave
+else
+	fLog "parse master status failed";
+	exit 3;
+fi
 
-#check status. if slave did not catch up in max cTimeoutSecs send warning email, exit error.
+
