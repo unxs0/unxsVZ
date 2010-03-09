1f4ada3710bada0571aabca00c03addd89722882
Gary <ggw@unixservice.com>
2010-03-09 13:38:49 +0000

    unxsBind updating for new unxsVZ standard logging.

--- unxsBind/interfaces/thit/bind9-genstats.sh
+++ unxsBind/interfaces/thit/bind9-genstats.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+#
+#FILE
+#	unxsBind/agents/thit/bind9-genstats.sh
+#PURPOSE
+#	Gather DNS statistics via rndc, named-stats and then run our own
+#	/usr/sbin/tHitCollector to parse and place data in MySQL db.
+#NOTES
+
 
 #Place next line in root crontab for iDNS tHit subsystem
-#*/5 * * * * /usr/sbin/bind9-genstats.sh >> /var/log/idns-cron.log 2>&1
+#*/5 * * * * /usr/sbin/bind9-genstats.sh >> /var/log/unxsbindlog 2>&1
+
+#unxsVZ standard log format
+fLog() { echo "`date +%b' '%d' '%T` $0[$$]: $@"; }
+
 
 #Configure
 STAT_FILE=/usr/local/idns/named.d/named.stats;
@@ -14,7 +26,7 @@ rm $STAT_FILE;
 $RNDC stats;
 RNDC_RET=$?;
 if [ $RNDC_RET -ne 0 ]; then
-	echo "Error running $RNDC:$RNDC_RET";
+	fLog "Error running $RNDC:$RNDC_RET";
 	exit $RNDC_RET;
 fi
 
@@ -22,7 +34,7 @@ fi
 $THIT;
 THIT_RET=$?;
 if [ $THIT_RET -ne 0 ]; then
-	echo "Error running $THIT:$THIT_RET";
+	fLog "Error running $THIT:$THIT_RET";
 	exit $THIT_RET;
 fi
 
