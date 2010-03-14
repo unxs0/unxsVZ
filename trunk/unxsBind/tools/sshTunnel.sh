3fad5600009091be02c58eccf2f54ab4e4980055
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2010-03-14 00:01:00 +0000

    unxsBind sshTunnel.sh enhanced and commented tests ok.

--- trunk/unxsBind/tools/sshTunnel.sh
+++ trunk/unxsBind/tools/sshTunnel.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
-#Setup a tunnel to not have to deal with MySQL SSL certs etc.
+#Setup a background tunnel to not have to deal with MySQL SSL certs etc.
+#
 #Note that the remote MySQL server will most likey need a GRANT ALL for 
 #the userbelow@at-server-itself since MySQL will see the connection as TCP from
 #it's own IP. And not use the socket.
 #
-#Not sure if ssh can use a socket on the remote sice of the -L
+#Note that this example will only work with ssh keygen setup pwd'less connections
+#for the user in question. In this case root.
+#
 
-/usr/bin/ssh -f -N -L 3307:node2vm:3306 root@node2vm
+#We specifcally bind to 127.0.0.1 and not use IPv6
+/usr/bin/ssh -4 -f -N -L 127.0.0.1:3307:node2vm:3306 root@node2vm
