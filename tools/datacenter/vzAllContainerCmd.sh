560c23bc19a50abd84a9da25562145d4733e46b7
Gary <ggw@unixservice.com>
2010-03-24 23:13:24 +0000

    unxsVZ datacenter/vzAllContainerCmd.sh beta working.

--- tools/datacenter/vzAllContainerCmd.sh
+++ tools/datacenter/vzAllContainerCmd.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 #FILE
-#	vzAllContainerCmd.sh
+#       vzAllContainerCmd.sh
 #PURPOSE
-#	Using vzlist run a command after vzctl enter into each one.
+#       Using vzlist run a command after vzctl enter into each one.
 #AUTHOR
-#	(C) 2010 Gary Wallis for Unixservice, LLC.
-#	GPLv2 license applies. See LICENSE file.
+#       (C) 2010 Gary Wallis for Unixservice, LLC.
+#       GPLv2 license applies. See LICENSE file.
 #
 #NOTES
 #
 #TODO
-#	
+#
 
-#Set these constants, make sure any tables they refer to have the correct data.
-cVEIDStatus="running";
-#      CTID      NPROC STATUS  IP_ADDR         HOSTNAME                        
-#       181         48 running 174.34.136.100 ns1.singtone.com  
-/usr/sbin/vzlist | \
+if [ "$1" == "" ];then
+	echo "usage: $0 command arg0 arg1...argN";
+	exit 0;
+fi
+
+#      CTID      NPROC STATUS  IP_ADDR         HOSTNAME
+#       181         48 running 174.34.136.100 ns1.singtone.com
+/usr/sbin/vzlist | grep running | \
 while read cvzlist
 do
-	cVZStatus=`echo $cvzlist | awk -F' ' '{print $3}'`;
-	if [ "$cVZStatus" != "$cVEIDStatus" ]; then
-		continue;
-	fi
-	uContainer=`echo $cvzlist | awk -F' ' '{print $1}'`;
-	cIP=`echo $cvzlist | awk -F' ' '{print $4}'`;
-	cHostname=`echo $cvzlist | awk -F' ' '{print $5}'`;
-	cLabel=`echo $cHostname | awk -F. '{print $1}'`;
-	cOSTemplate=`cat /etc/vz/conf/$uContainer.conf | grep OSTEMPLATE | awk -F= '{print $2}' | awk -F'"' '{print $2}'`;
-	cConfig=`cat /etc/vz/conf/$uContainer.conf | grep ORIGIN_SAMPLE | awk -F= '{print $2}' | awk -F'"' '{print $2}'`;
+        uContainer=`echo $cvzlist | awk -F' ' '{print $1}'`;
+        #cIP=`echo $cvzlist | awk -F' ' '{print $4}'`;
+        cHostname=`echo $cvzlist | awk -F' ' '{print $5}'`;
+        #cLabel=`echo $cHostname | awk -F. '{print $1}'`;
+        #cOSTemplate=`cat /etc/vz/conf/$uContainer.conf | grep OSTEMPLATE | awk -F= '{print $2}' | awk -F'"' '{print $2}'`;
+        #cConfig=`cat /etc/vz/conf/$uContainer.conf | grep ORIGIN_SAMPLE | awk -F= '{print $2}' | awk -F'"' '{print $2}'`;
 
 
-	echo uContainer=$uContainer;
-	echo cLabel=$cLabel;
-	echo cHostname=$cHostname;
-	echo cIP=$cIP;
-	echo cOSTemplate=$cOSTemplate;
-	echo cConfig=$cConfig;
+        echo uContainer=$uContainer cHostname=$cHostname;
+        #echo cLabel=$cLabel;
+        #echo cHostname=$cHostname;
+        #echo cIP=$cIP;
+        #echo cOSTemplate=$cOSTemplate;
+        #echo cConfig=$cConfig;
 
-	/usr/sbin/vzctl exec $uContainer ls; 
+        /usr/sbin/vzctl exec2 $uContainer $1 < /dev/tty;
 
 done
 
