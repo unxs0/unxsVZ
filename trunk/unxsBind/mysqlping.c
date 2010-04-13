0407452d24944e400b8e0c9877784ea0ffd09349
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2010-04-13 22:24:00 +0000

    unxsBind mysqlping test code for two servers seems to work and be fast enough for production use.

--- trunk/unxsBind/mysqlping.c
+++ trunk/unxsBind/mysqlping.c
@@ -2,12 +2,12 @@
 FILE
 	$Id$
 PURPOSE
-	Test verifying which server is reachable before mysql connect;
+	Test verifying which server is reachable before mysql_real_connect;
 AUTHOR
 	(C) 2010 Gary Wallis for Unixservice, LLC.
 NOTES
-	We must be able to handle mixed local socket and IP based
-	connections. TODO
+	mysql_real_connect() handles the local PF_UNIX just fine, so we can ignore pre 
+	testing in those cases.
 */
 
 #include "mysqlrad.h"
@@ -22,87 +22,101 @@ MYSQL gMysql;
 int main()
 {
 
-	char *cIP0="127.0.0.1";
-	char *cPort="3306";
-	char *cIP1="127.0.0.1";
+	char *cPort="3306";//(*1)
 	int sock;
-	int domain0=PF_INET;//PF_UNIX
-	int domain1=PF_INET;//PF_UNIX
-	unsigned uUseIP=0;
 	struct sockaddr_in sockaddr_inMySQLServer;
 
-	if(DBPORT==0)
-		cPort="3306";
-	else
+	//Default port should really be gathered from a different source
+	//but for now we use the known MySQL server CentOS default port (*1).
+	if(DBPORT!=0)
 		sprintf(cPort,"%u",DBPORT);
 
-	if(DBIP0==NULL)
-		cIP0="127.0.0.1";
-	else
-		cIP0=DBIP0;
-
-	if(DBIP1==NULL)
-		cIP1="127.0.0.1";
-	else
-		cIP1=DBIP1;
-
-
+	//Handle quick cases first
+	//Port is irrelevant here. Make it clear.
 	mysql_init(&gMysql);
-
-	//Determine which IP is up at given port
-	//DBIP0
-	if((sock=socket(domain0,SOCK_STREAM,IPPROTO_TCP))<0)
+	if(DBIP0==NULL)
 	{
-		printf("Could not create test ip:port socket\n");
+		if (mysql_real_connect(&gMysql,DBIP0,DBLOGIN,DBPASSWD,DBNAME,0,DBSOCKET,0))
+		{
+			printf("Connected to local socket DBIP0==NULL\n");
+			mysql_close(&gMysql);
+			exit(0);
+		}
+	}
+	if(DBIP1==NULL)
+	{
+		if (mysql_real_connect(&gMysql,DBIP1,DBLOGIN,DBPASSWD,DBNAME,0,DBSOCKET,0))
+		{
+			printf("Connected to local socket DBIP1==NULL\n");
+			mysql_close(&gMysql);
+			exit(0);
+		}
+	}
+
+	//Now we can use AF_INET/IPPROTO_TCP cases (TCP connections via IP number)
+
+	//This goes first. Since if it doesn't work we can't really use
+	//anything that follows
+	if((sock=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP))<0)
+	{
+		printf("Could not create socket\n");
 		exit(1);
 	}
-	//Depending on domain0 this section varies
-	memset(&sockaddr_inMySQLServer, 0, sizeof(sockaddr_inMySQLServer));
-	sockaddr_inMySQLServer.sin_family = AF_INET;
-	sockaddr_inMySQLServer.sin_addr.s_addr = inet_addr(cIP0);
-	sockaddr_inMySQLServer.sin_port = htons(atoi(cPort));
-	if(connect(sock,(struct sockaddr *) &sockaddr_inMySQLServer,sizeof(sockaddr_inMySQLServer))<0)
+
+
+	if(DBIP0!=NULL)
 	{
-		uUseIP=0;
-	}
-	else
-	{
-		//DBIP1
-		close(sock);
-		if((sock=socket(domain1,SOCK_STREAM,IPPROTO_TCP))<0)
+		//DBIP0 has priority if we can create a connection we
+		//move forward immediately.
+		memset(&sockaddr_inMySQLServer,0,sizeof(sockaddr_inMySQLServer));
+		sockaddr_inMySQLServer.sin_family=AF_INET;
+		sockaddr_inMySQLServer.sin_addr.s_addr=inet_addr(DBIP0);
+		sockaddr_inMySQLServer.sin_port=htons(atoi(cPort));
+		if(connect(sock,(struct sockaddr *)&sockaddr_inMySQLServer,sizeof(sockaddr_inMySQLServer))>=0)
 		{
-			printf("Could not create test ip:port socket\n");
-			exit(1);
-		}
-		//Depending on domain1 this section varies
-		memset(&sockaddr_inMySQLServer, 0, sizeof(sockaddr_inMySQLServer));
-		sockaddr_inMySQLServer.sin_family = AF_INET;
-		sockaddr_inMySQLServer.sin_addr.s_addr = inet_addr(cIP1);
-		sockaddr_inMySQLServer.sin_port = htons(atoi(cPort));
-		if(connect(sock,(struct sockaddr *) &sockaddr_inMySQLServer,sizeof(sockaddr_inMySQLServer))<0)
-		{
-			uUseIP=1;
+			close(sock);//Don't need anymore.
+			mysql_init(&gMysql);
+			if(mysql_real_connect(&gMysql,DBIP0,DBLOGIN,DBPASSWD,DBNAME,DBPORT,DBSOCKET,0))
+			{
+				printf("Connected to %s:%s\n",(char *)DBIP0,cPort);
+				mysql_close(&gMysql);
+				exit(0);
+			}
 		}
 	}
-	close(sock);
 
-	//Depending on live IP try to connect
-	if(uUseIP)
+	if(DBIP1!=NULL)
 	{
-		if (!mysql_real_connect(&gMysql,DBIP1,DBLOGIN,DBPASSWD,DBNAME,DBPORT,DBSOCKET,0))
-			exit(1);
-		else
-			printf("Connected to %s:%s\n",cIP1,cPort);
-	}
-	else
-	{
-		if (!mysql_real_connect(&gMysql,DBIP0,DBLOGIN,DBPASSWD,DBNAME,DBPORT,DBSOCKET,0))
-			exit(1);
-		else
-			printf("Connected to %s:%s\n",cIP0,cPort);
+		//Fallback to DBIP1
+		memset(&sockaddr_inMySQLServer,0,sizeof(sockaddr_inMySQLServer));
+		sockaddr_inMySQLServer.sin_family=AF_INET;
+		sockaddr_inMySQLServer.sin_addr.s_addr=inet_addr(DBIP1);
+		sockaddr_inMySQLServer.sin_port=htons(atoi(cPort));
+		if(connect(sock,(struct sockaddr *)&sockaddr_inMySQLServer,sizeof(sockaddr_inMySQLServer))>=0)
+		{
+			close(sock);//Don't need anymore.
+			mysql_init(&gMysql);
+			if(mysql_real_connect(&gMysql,DBIP1,DBLOGIN,DBPASSWD,DBNAME,DBPORT,DBSOCKET,0))
+			{
+				printf("Connected to %s:%s\n",(char *)DBIP1,cPort);
+				mysql_close(&gMysql);
+				exit(0);
+			}
+		}
 	}
 
-	mysql_close(&gMysql);
-	exit(0);
+	//Failure exit 4 cases
+	if(DBIP1!=NULL && DBIP0!=NULL)
+		printf("Could not connect to %s:%s or %s:%s\n",(char *)DBIP0,cPort,(char *)DBIP1,cPort);
+	else if(DBIP1==NULL && DBIP0==NULL)
+		printf("Could not connect. Tried to use local socket\n");
+	else if(DBIP0!=NULL && DBIP1==NULL)
+		printf("Could not connect to %s:%s or local socket (DBIP1)\n",(char *)DBIP0,cPort);
+	else if(DBIP0==NULL && DBIP1!=NULL)
+		printf("Could not connect to %s:%s or local socket (DBIP0)\n",(char *)DBIP1,cPort);
+	else if(1)
+		printf("Could not connect unexpected case\n");
+	
+	exit(1);
 
 }//main()
