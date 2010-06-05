010ed531a9a03658271f0bd325dde4f5acd3a24a
Gary <ggw@unixservice.com>
2010-06-05 12:59:59 +0000

    unxsVZ some work on aux bash backup scripts.

--- tools/rsync/bodbackup.sh
+++ tools/rsync/bodbackup.sh
@@ -14,36 +14,53 @@
 #	Non LVM2 version! But should be very similar.
 #	In some hot swap SATA servers the server will boot from an unknown drive/part
 #	so we use the unique hdparm serial numbers for HD id.
+#	We could use UUID instead of the serial number.
+#
+#TODO
+#	Set everything up with top constants section.
 
 #This servers drives
 HD0Serial="8SF0JGS9";
 HD1Serial="8SF0JJNY";
 
-GotSerial0=`/sbin/hdparm -i /dev/sda | grep SerialNo | awk '{print $6}'`;
-GotSerial1=`/sbin/hdparm -i /dev/sdb | grep SerialNo | awk '{print $6}'`;
+DRIVE0="/dev/sda";
+DRIVE1="/dev/sdb";
+
+BAKROOTDEV="/dev/sdb1";
+BAKVZDEV="/dev/sdb3";
+
+SRCROOTDEV="/dev/sda1";
+SRCVZDEV="/dev/sda3";
+
+#Assumed partitions / and /vz
+
+#Nothing below should need to be changed.
+
+GotSerial0=`/sbin/hdparm -i $DRIVE0 | grep SerialNo | awk '{print $6}'`;
+GotSerial1=`/sbin/hdparm -i $DRIVE1 | grep SerialNo | awk '{print $6}'`;
 
 if [ $HD0Serial == $GotSerial0 ];then
-        echo "/dev/sda serial number ok";
+        echo "$DRIVE0 serial number ok";
 else
-        echo "/dev/sda serial number does not match our records, drives swapped?";
+        echo "$DRIVE0 serial number does not match our records, drives swapped?";
 
         if [ $HD0Serial == $GotSerial1 ];then
                 echo "Drives swapped";
         else
-                echo "Error! /dev/sda Serial numbers do not match our records!";
+                echo "Error! $DRIVE0 serial numbers do not match our records!";
                 exit 1;
         fi
 fi
 
 if [ $HD1Serial == $GotSerial1 ];then
-        echo "/dev/sdb serial number ok";
+        echo "$DRIVE1 serial number ok";
 else
-        echo "/dev/sdb serial number does not match our records, drives swapped?";
+        echo "$DRIVE1 serial number does not match our records, drives swapped?";
 
         if [ $HD1Serial == $GotSerial0 ];then
                 echo "Drives swapped";
         else
-                echo "Error! /dev/sdb Serial numbers do not match our records!";
+                echo "Error! $DRIVE1 serial numbers do not match our records!";
                 exit 1;
         fi
 fi
@@ -51,29 +68,29 @@ fi
 #Now make sure we are not cross mounted.
 ROOTDEV=`/bin/mount | grep -w "on /" | awk '{print $1}'`;
 VZDEV=`/bin/mount | grep -w "on /vz" | awk '{print $1}'`;
-BAKROOTDEV="/dev/sdb1";
-BAKVZDEV="/dev/sdb3";
 
-if [ $ROOTDEV == "/dev/sda1" ];then
-        echo "/dev/sda1 is mounted as expected";
+if [ $ROOTDEV == $SRCROOTDEV ];then
+        echo " $SRCROOTDEV is mounted as expected";
 else
         if [ $ROOTDEV == $BAKROOTDEV ];then
-                echo "Warning /dev/sdb1 is mounted on /";
-                BAKROOTDEV="/dev/sda1";
+                echo "Warning $BAKROOTDEV is mounted on /";
+		#switch
+                BAKROOTDEV="$SRCROOTDEV";
         else
-                echo "Error! unexpected device is mounted on / aborting";
+                echo "Error! unexpected device $ROOTDEV is mounted on / aborting";
                 exit 1;
         fi
 fi
 
-if [ $VZDEV == "/dev/sda3" ];then
+if [ $VZDEV == $SRCVZDEV ];then
         echo "/dev/sda3 is mounted as expected";
 else
         if [ $VZDEV == $BAKVZDEV ];then
-                echo "Warning /dev/sdb3 is mounted on /vz";
-                BAKVZDEV="/dev/sda3";
+                echo "Warning $BAKVZDEV is mounted on /vz";
+		#switch
+                BAKVZDEV="$SRCVZDEV";
         else
-                echo "Error! unexpected device is mounted on /vz aborting";
+                echo "Error! unexpected device $VZDEV is mounted on /vz aborting";
                 exit 1;
         fi
 fi
@@ -103,6 +120,12 @@ fi
 
 echo ""
 echo "mounting..."
+/bin/mount $BAKVZDEV /mnt/vz
+if [ $? -gt 0 ]; then
+        echo "Could not mount $BAKVZDEV. Aborting";
+        exit 1;
+fi
+
 /bin/mount $BAKROOTDEV /mnt/root
 if [ $? -gt 0 ]; then
         echo "Could not mount $BAKROOTDEV. Aborting";
@@ -113,7 +136,9 @@ fi
 
 #disk must be mounted or else we 100% the root partition!
 
-/bin/sync;
+#this command may take forever in systems like mail stores with many open files.
+#may not be needed since we should only do best effort not perfect backup.
+#/bin/sync;
 
 echo ""
 echo "rsync vz partition..."
