e1a266bd5defd0b9c2d247e6ffd17bb349be5ecf
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2010-06-05 15:25:53 +0000

    unxsVZ new UUID version of BOD rsync backup script.

--- trunk/tools/rsync/bodbackup.sh
+++ trunk/tools/rsync/bodbackup.sh
@@ -3,79 +3,70 @@
 #FILE
 #	tools/rsync/bodbackup.sh
 #PURPOSE
-#	unxsVZ server backup script that allows for boot from snapshot drive if primary drive
-#	fails. This requires that the drives be identical and setup correctly (/boot fdisk etc.)
-#
-#	A simple KISS example that however has worked in production.
-#	Here for /dev/sda and /dev/sdb 2 drive server.
-#	Bunch of drives (bod) local rsync to unmounted "mirror" snapshot drive on same
-#	server.
-#NOTES
-#	Non LVM2 version! But should be very similar.
-#	In some hot swap SATA servers the server will boot from an unknown drive/part
-#	so we use the unique hdparm serial numbers for HD id.
-#	We could use UUID instead of the serial number.
-#
-#TODO
-#	Set everything up with top constants section.
+#	Custom server backup script that keeps a daily backup drive ready for emergency boot.
+#	This requires that the drives be identical and setup correctly (MBR grub fdisk etc.)
+#	Care is taken to not run if drives get cross mounted or other abnormal situations.
 
 #This servers drives
-HD0Serial="8SF0JGS9";
-HD1Serial="8SF0JJNY";
+HD0UUID="6b51bc8c-ca03-4678-b585-e122b2236559";
+HD1UUID="fd952d79-dcd4-49fd-9e54-0f452f889a3d";
 
-DRIVE0="/dev/sda";
-DRIVE1="/dev/sdb";
+UUIDPART0="/dev/sda1";
+UUIDPART1="/dev/sdb1";
 
-BAKROOTDEV="/dev/sdb1";
-BAKVZDEV="/dev/sdb3";
+SRCROOTDEV="/dev/mapper/VolGroup00-LogVol00";
+SRCVZDEV="/dev/mapper/VolGroup00-LogVol03";
 
-SRCROOTDEV="/dev/sda1";
-SRCVZDEV="/dev/sda3";
+BAKROOTDEV="/dev/mapper/VolGroup01-LogVol00";
+BAKVZDEV="/dev/mapper/VolGroup01-LogVol03";
 
 #Assumed partitions / and /vz
 
 #Nothing below should need to be changed.
 
-GotSerial0=`/sbin/hdparm -i $DRIVE0 | grep SerialNo | awk '{print $6}'`;
-GotSerial1=`/sbin/hdparm -i $DRIVE1 | grep SerialNo | awk '{print $6}'`;
+GotUUID0=`/lib/udev/vol_id $UUIDPART0 | grep UUID | cut -f 2 -d =`;
+GotUUID1=`/lib/udev/vol_id $UUIDPART1 | grep UUID | cut -f 2 -d =`;
 
-if [ $HD0Serial == $GotSerial0 ];then
-        echo "$DRIVE0 serial number ok";
+if [ $HD0UUID == $GotUUID0 ];then
+        echo "$UUIDPART0 UUID number ok";
 else
-        echo "$DRIVE0 serial number does not match our records, drives swapped?";
+        echo "$UUIDPART0 UUID number does not match our records, drives swapped?";
 
-        if [ $HD0Serial == $GotSerial1 ];then
+        if [ $HD0UUID == $GotUUID1 ];then
                 echo "Drives swapped";
         else
-                echo "Error! $DRIVE0 serial numbers do not match our records!";
+                echo "Error! $UUIDPART0 UUID numbers do not match our records!";
                 exit 1;
         fi
 fi
 
-if [ $HD1Serial == $GotSerial1 ];then
-        echo "$DRIVE1 serial number ok";
+if [ $HD1UUID == $GotUUID1 ];then
+        echo "$UUIDPART1 UUID number ok";
 else
-        echo "$DRIVE1 serial number does not match our records, drives swapped?";
+        echo "$UUIDPART1 UUID number does not match our records, drives swapped?";
 
-        if [ $HD1Serial == $GotSerial0 ];then
+        if [ $HD1UUID == $GotUUID0 ];then
                 echo "Drives swapped";
         else
-                echo "Error! $DRIVE1 serial numbers do not match our records!";
+                echo "Error! $UUIDPART1 UUID numbers do not match our records!";
                 exit 1;
         fi
 fi
 
-#Now make sure we are not cross mounted.
+#Check to see if we are cross mounted. This can happen on some servers
+#solution use UUID in grub.conf
+#We do not allow cross mounts.
 ROOTDEV=`/bin/mount | grep -w "on /" | awk '{print $1}'`;
 VZDEV=`/bin/mount | grep -w "on /vz" | awk '{print $1}'`;
 
 if [ $ROOTDEV == $SRCROOTDEV ];then
-        echo " $SRCROOTDEV is mounted as expected";
+        echo "$SRCROOTDEV is mounted as expected";
 else
         if [ $ROOTDEV == $BAKROOTDEV ];then
-                echo "Warning $BAKROOTDEV is mounted on /";
+                echo "Error $BAKROOTDEV is mounted on /";
+		exit 1;
 		#switch
-                BAKROOTDEV="$SRCROOTDEV";
+                #BAKROOTDEV="$SRCROOTDEV";
         else
                 echo "Error! unexpected device $ROOTDEV is mounted on / aborting";
                 exit 1;
@@ -83,12 +74,13 @@ else
 fi
 
 if [ $VZDEV == $SRCVZDEV ];then
-        echo "/dev/sda3 is mounted as expected";
+        echo "$SRCVZDEV is mounted as expected";
 else
         if [ $VZDEV == $BAKVZDEV ];then
-                echo "Warning $BAKVZDEV is mounted on /vz";
+                echo "Error $BAKVZDEV is mounted on /vz";
+		exit 1;
 		#switch
-                BAKVZDEV="$SRCVZDEV";
+                #BAKVZDEV="$SRCVZDEV";
         else
                 echo "Error! unexpected device $VZDEV is mounted on /vz aborting";
                 exit 1;
@@ -104,7 +96,6 @@ fi
 umount $BAKROOTDEV > /dev/null 2>&1;
 umount $BAKVZDEV > /dev/null 2>&1;
 
-echo ""
 echo "mkdirs..."
 /bin/mkdir -p /mnt/root
 if [ $? -gt 0 ]; then
@@ -118,7 +109,6 @@ if [ $? -gt 0 ]; then
         exit 1;
 fi
 
-echo ""
 echo "mounting..."
 /bin/mount $BAKVZDEV /mnt/vz
 if [ $? -gt 0 ]; then
@@ -134,30 +124,28 @@ if [ $? -gt 0 ]; then
         exit 1;
 fi
 
-#disk must be mounted or else we 100% the root partition!
-
-#this command may take forever in systems like mail stores with many open files.
-#may not be needed since we should only do best effort not perfect backup.
-#/bin/sync;
+#debug only
+#exit 0;
 
 echo ""
 echo "rsync vz partition..."
-/usr/bin/rsync -avxlH --delete /vz/* /mnt/vz
+/usr/bin/rsync -avxH --delete /vz/* /mnt/vz
 
 echo ""
 echo "rsync root partition..."
-/usr/bin/rsync -avxlH --delete --exclude "/mnt/root/" --exclude "/mnt/vz/" --exclude "/vz" --exclude "/proc/" --exclude "/root/.ccache/" --exclude "/sys" --exclude "/dev" --exclude "/tmp" /* /mnt/root
+#make sure /boot is a diff partition we don't want to clobber "/boot/grub/grub.conf"
+#just to be safe we exclude anyway
+/usr/bin/rsync -avxH --delete --exclude "/mnt/root/" --exclude "/mnt/vz/" --exclude "/vz" --exclude "/proc/" --exclude "/root/.ccache/" --exclude "/sys" --exclude "/dev" --exclude "/tmp" --exclude "/boot/grub/grub.conf" /* /mnt/root
 
 echo ""
 echo "un-mounting..."
 /bin/umount /mnt/root
 /bin/umount /mnt/vz
 
-echo ""
 echo "rm temp /mnt dirs..."
 /bin/rmdir /mnt/root
 /bin/rmdir /mnt/vz
 
-echo ""
 echo "done"
+exit 0;
 
