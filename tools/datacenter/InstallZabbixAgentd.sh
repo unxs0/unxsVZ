50ebec5e40cead3c39234b4d7a6d55ca0eedfdfb
Gary <ggw@unixservice.com>
2010-11-05 22:49:44 +0000

    unxsVZ tools/datacenter/InstallZabbixAgentd.sh updated version commit.

--- tools/datacenter/InstallZabbixAgentd.sh
+++ tools/datacenter/InstallZabbixAgentd.sh
@@ -11,10 +11,30 @@
 #NOTES
 #
 #TODO
+#	Might be nice to have an option to not install unless a valid DNS A record
+#	exists (resolves) and is the same as the vzlist IP_ADDR.
+#
+#	Would also be good to not reinstall if agent is already installed.
+#	Maybe only insure it is running in that case?
 #
 
 if [ "$1" != "install" ];then
 	echo "usage: $0 install [<hostname>]";
+	exit 0;
+fi
+
+if [ ! -f /usr/local/sbin/zabbix_agentd ];then
+	echo "/usr/local/sbin/zabbix_agentd not installed";
+	exit 0;
+fi
+
+if [ ! -f /etc/init.d/zabbix_agentd ];then
+	echo "/etc/init.d/zabbix_agentd not installed";
+	exit 0;
+fi
+
+if [ ! -f ./zabbix_agentd.conf ];then
+	echo "./zabbix_agentd.conf not installed";
 	exit 0;
 fi
 
@@ -25,6 +45,8 @@ while read cvzlist
 do
         uContainer=`echo $cvzlist | awk -F' ' '{print $1}'`;
         cHostname=`echo $cvzlist | awk -F' ' '{print $5}'`;
+        cIPAddr=`echo $cvzlist | awk -F' ' '{print $4}'`;
+	cPrevInstalled="n";
 
 	if [ "$2" != "" ];then
 		if [[ ! "$cHostname" =~ $2 ]];then
@@ -33,8 +55,28 @@ do
 	fi
 
         echo $uContainer:$cHostname;
+	cARecord=`/usr/bin/dig $cHostname A +short`;
+	if [ $? != 0 ];then
+        	echo "dig failed for $cHostname";
+		continue;
+	fi
+	if [ "$cIPAddr" != "$cARecord" ];then
+		echo "$cARecord does not match $cIPAddr";
+		continue;
+	fi
+
+	#debug only
+	#echo "$cARecord matches $cIPAddr";
+	#continue;
+	#just in case ;)
+	#exit;
 
 	#from outside
+
+	#if previously installed
+	if [ -f /vz/private/$uContainer/etc/zabbix/zabbix_agentd.conf ];then
+		cPrevInstalled="y";
+	fi
 
 	#install agent binary
 	cp /usr/local/sbin/zabbix_agentd\
@@ -53,13 +95,22 @@ do
 	#create zabbix user and group
 	#set perms
 	#start agent
-        /usr/sbin/vzctl exec2 $uContainer 'groupadd zabbix;\
+	if [ "$cPrevInstalled" == "n" ];then
+	        /usr/sbin/vzctl exec2 $uContainer 'groupadd zabbix;\
 						useradd -g zabbix -s /sbin/nologin zabbix;\
 						chown zabbix.zabbix /etc/zabbix/;\
 						touch /var/log/zabbix_agentd.log;\
 						chown zabbix.zabbix /var/log/zabbix_agentd.log;\
 						/etc/init.d/zabbix_agentd start;\
 						/sbin/chkconfig --level 3 zabbix_agentd on; ' < /dev/tty;
+	else
+		#we assume that the first install was done correctly DANGER
+		#and that this is just an update like after a hostname change etc.
+		#or we install a better conf or init or new agentd
+	        /usr/sbin/vzctl exec2 $uContainer '/etc/init.d/zabbix_agentd stop;\
+						sleep 1;\
+						/etc/init.d/zabbix_agentd start; ' < /dev/tty;
+	fi
 
 done
 
