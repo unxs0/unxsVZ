b5659ca789f5d26b2856a2dd4bda82003f48acba
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2011-02-10 17:19:57 +0000

    unxsVZ openSIPS agent being extended for job queue handling.

--- trunk/agents/opensips/sips.c
+++ trunk/agents/opensips/sips.c
@@ -18,7 +18,7 @@ MYSQL gMysqlExt;
 char gcQuery[8192]={""};
 unsigned guLoginClient=1;//Root user
 char cHostname[100]={""};
-char gcProgram[32]={""};
+char gcProgram[100]={""};
 unsigned guNodeOwner=1;
 unsigned guContainerOwner=1;
 unsigned guStatus=0;//not a valid status
@@ -28,6 +28,7 @@ void TextConnectDb(void);
 
 //local protos
 void ProcessDR(void);
+void unxsVZJobs(void);
 void TextConnectOpenSIPSDb(void);
 
 static FILE *gLfp=NULL;
@@ -46,12 +47,12 @@ void logfileLine(const char *cFunction,const char *cLogline,const unsigned uCont
 		tmTime=localtime(&luClock);
 		strftime(cTime,31,"%b %d %T",tmTime);
 
-		fprintf(gLfp,"%s %s[%u]: %s. uContainer=%u\n",cTime,cFunction,pidThis,cLogline,uContainer);
+		fprintf(gLfp,"%s unxsSIPS::%s[%u]: %s. uContainer=%u\n",cTime,cFunction,pidThis,cLogline,uContainer);
 		fflush(gLfp);
 	}
 	else
 	{
-		fprintf(stderr,"%s: %s. uContainer=%u\n",cFunction,cLogline,uContainer);
+		fprintf(stderr,"%s: unxsSIPS::%s. uContainer=%u\n",cFunction,cLogline,uContainer);
 	}
 
 }//void logfileLine()
@@ -61,11 +62,11 @@ int main(int iArgc, char *cArgv[])
 {
 	struct sysinfo structSysinfo;
 
-	sprintf(gcProgram,"%.31s",cArgv[0]);
+	sprintf(gcProgram,"%.99s",cArgv[0]);
 
 	if((gLfp=fopen(cLOGFILE,"a"))==NULL)
 	{
-		logfileLine("ProcessDR","fopen logfile failed",0);
+		logfileLine("main","fopen logfile failed",0);
 		exit(1);
 	}
 
@@ -86,18 +87,160 @@ int main(int iArgc, char *cArgv[])
 	//will avoid runaway du and other unexpected high load
 	//situations. See #120.
 
-	ProcessDR();
+	if(iArgc==2)
+	{
+		if(!strncmp(cArgv[1],"ProcessDR",9))
+		{
+			ProcessDR();
+			goto CommonExit;
+		}
+		else if(!strncmp(cArgv[1],"unxsVZJobs",10))
+		{
+			unxsVZJobs();
+			goto CommonExit;
+		}
+	}
 
+	printf("Usage: %s ProcessDR|unxsVZJobs\n",gcProgram);
+
+CommonExit:
+	fclose(gLfp);
 	return(0);
+
 }//main()
 
 
-void ProcessDR(void)
+void unxsVZJobs(void)
 {
         MYSQL_RES *res;
         MYSQL_ROW field;
         MYSQL_RES *res2;
         MYSQL_ROW field2;
+
+	//Uses login data from local.h
+	TextConnectDb();
+	TextConnectOpenSIPSDb();
+	guLoginClient=1;//Root user
+
+	sprintf(gcQuery,"SELECT uJob,uDatacenter,uNode,uContainer,uOwner,cJobName,cJobData FROM tJob"
+			" WHERE uJobStatus=10"
+			" AND cJobName LIKE 'unxsSIPS%%'"
+			" AND uJobDate<=UNIX_TIMESTAMP(NOW()) LIMIT 1");
+	mysql_query(&gMysql,gcQuery);
+	if(mysql_errno(&gMysql))
+	{
+		logfileLine("unxsVZJobs",mysql_error(&gMysql),0);
+		mysql_close(&gMysql);
+		exit(2);
+	}
+        res=mysql_store_result(&gMysql);
+	if((field=mysql_fetch_row(res)))
+	{
+		char *cp;
+		char *cp2;
+		char cDID[16]={""};
+		char cHostname[64]={""};
+		char cJobName[33]={""};
+		unsigned uJob=0;
+		unsigned uDatacenter=0;
+		unsigned uNode=0;
+		unsigned uContainer=0;
+		unsigned uOwner=0;
+		unsigned uGwid=0;//OpenSIPS schema
+
+		sscanf(field[0],"%u",&uJob);
+		sscanf(field[1],"%u",&uDatacenter);
+		sscanf(field[2],"%u",&uNode);
+		sscanf(field[3],"%u",&uContainer);
+		sscanf(field[4],"%u",&uOwner);
+		sprintf(cJobName,"%.32s",field[5]);
+
+		if(!strncmp(cJobName,"unxsSIPSNewDID",14))
+		{
+
+			if((cp=strstr(field[6],"cDID=")))
+			{
+				if((cp2=strchr(cp+5,';')))
+				{
+					*cp2=0;
+					sprintf(cDID,cp+5);
+					*cp2=';';
+				}
+			}
+			if((cp=strstr(field[6],"cHostname=")))
+			{
+				if((cp2=strchr(cp+10,';')))
+				{
+					*cp2=0;
+					sprintf(cHostname,cp+10);
+					*cp2=';';
+				}
+			}
+
+			//Make sure PBX is registered
+			sprintf(gcQuery,"SELECT gwid,attrs FROM dr_gateways WHERE type=1 AND address='%s'",cHostname);
+			mysql_query(&gMysqlExt,gcQuery);
+			if(mysql_errno(&gMysqlExt))
+			{
+				logfileLine("unxsVZJobs",mysql_error(&gMysqlExt),uContainer);
+				mysql_close(&gMysql);
+				mysql_close(&gMysqlExt);
+				exit(2);
+			}
+			res2=mysql_store_result(&gMysqlExt);
+			if((field2=mysql_fetch_row(res2)))
+				sscanf(field2[0],"%u",&uGwid);
+			mysql_free_result(res2);
+
+			if(uGwid)
+			{
+				//Add new DID only if not already in table
+				sprintf(gcQuery,"SELECT prefix FROM dr_rules WHERE gwlist='%u' AND prefix='%s'",
+								uGwid,cDID);
+				mysql_query(&gMysqlExt,gcQuery);
+				if(mysql_errno(&gMysqlExt))
+				{
+					logfileLine("ProcessDR",mysql_error(&gMysqlExt),uContainer);
+					mysql_close(&gMysql);
+					mysql_close(&gMysqlExt);
+					exit(2);
+				}
+				res2=mysql_store_result(&gMysqlExt);
+				if(mysql_num_rows(res2)==0)
+				{
+					//debug only
+					printf("Add %s for %s\n",cDID,cHostname);
+				}
+				else
+				{
+					//debug only
+					printf("%s for %s already in dr_rules\n",cDID,cHostname);
+				}
+			}
+			else
+			{
+				//debug only
+				printf("%s is not in dr_gateways\n",cHostname);
+			}
+
+		}//unxsSIPSNewDID
+	}
+	mysql_free_result(res);
+
+	mysql_close(&gMysql);
+	mysql_close(&gMysqlExt);
+	exit(0);
+
+
+}//void unxsVZJobs(void)
+
+
+void ProcessDR(void)
+{
+        MYSQL_RES *res2;
+        MYSQL_ROW field2;
+        MYSQL_RES *res;
+        MYSQL_ROW field;
 	unsigned uContainer=0;
 	unsigned uOwner=0;
 	unsigned uGwid=0;//OpenSIPS schema
