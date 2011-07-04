35a1733c49892682b8700d27c368c4a34e3d2c1a
Gary <ggw@unixservice.com>
2011-07-04 14:46:33 +0000

    unxsVZ mcs minor update.

--- tools/mcs/getcommand.sh
+++ tools/mcs/getcommand.sh
@@ -13,23 +13,26 @@ cMCSServerFQDN="mcs.example.com";
 cMCSServerPort="80";
 cMCSInstallPath="/usr/local/mcsremoteagent";
 
+fLog() { echo "`date +%b' '%d' '%T` $0[$$]: $@"; }
+
 if [ "$1" == "" ];then
-	echo "usage: $0 run";
-	exit;
+        echo "usage: $0 run";
+        exit;
 fi
 
 cHostname=`/bin/hostname -f`;
 if [ $? != 0 ];then
-        echo "cHostname error";
+        fLog "cHostname error";
         exit 1;
 fi
 
 cFile=`echo "$cHostname$cSharedSecret" | /usr/bin/md5sum | /bin/cut -f 1 -d " "`;
 if [ $? != 0 ];then
-        echo "cFile error";
+        fLog "cFile error";
         exit 1;
 fi
 
+#ignore # commented lines
 cResponse=`/usr/bin/wget --quiet --no-check-certificate --output-document=-\
          https://$cMCSServerFQDN:$cMCSServerPort/$cFile | grep -v "^\#"`;
 if [ $? != 0 ];then
@@ -39,36 +42,38 @@ fi
 #run
 echo $cResponse | grep -w "agentVoip=run;" > /dev/null 2>&1;
 if [ $? == 0 ];then
-        echo "attempt run";
+        #fLog "attempt run";
         ps -ef | grep -v grep | grep -w "rta.jar" > /dev/null 2>&1;
-        if [ $? == 0 ];then
-                echo "already running";
-        else
-                echo "start rta";
+        #if [ $? == 0 ];then
+        #       fLog "already running";
+        #else
+        if [ $? != 0 ];then
+                fLog "start rta";
                 $cMCSInstallPath/start_rta.sh > /dev/null 2>&1;
                 if [ $? != 0 ];then
-                        echo "$cMCSInstallPath/start_rta.sh error";
+                        fLog "$cMCSInstallPath/start_rta.sh error";
                         exit 1;
                 fi
-        	exit 0;
+                exit 0;
         fi
 fi
 
 #stop
 echo $cResponse | grep -w "agentVoip=stop;" > /dev/null 2>&1;
 if [ $? == 0 ];then
-        echo "attempt stop";
+        #fLog "attempt stop";
         ps -ef | grep -v grep | grep -w "rta.jar" > /dev/null 2>&1;
-        if [ $? != 0 ];then
-                echo "not running";
-        else
-                echo "stop rta";
+        #if [ $? != 0 ];then
+        #       fLog "not running";
+        #else
+        if [ $? == 0 ];then
+                fLog "stop rta";
                 $cMCSInstallPath/stop_rta.sh > /dev/null 2>&1;
                 if [ $? != 0 ];then
-                        echo "$cMCSInstallPath/stop_rta.sh error";
+                        fLog "$cMCSInstallPath/stop_rta.sh error";
                         exit 1;
                 fi
-        	exit 0;
+                exit 0;
         fi
 fi
 
