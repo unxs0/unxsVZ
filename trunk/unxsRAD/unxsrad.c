c38098b71bd8adda315ae8cf9acddd3ca178a575
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2012-05-31 02:11:07 +0000

    unxsRAD started work on CLI job queue executable.

---
+++ trunk/unxsRAD/unxsrad.c
@@ -0,0 +1,25 @@
+/*
+FILE 
+	unxsrad.c
+	$Id$
+PURPOSE
+	unxsRAD job queue CLI standalone program	
+LEGAL
+	(C) Gary Wallis 2012. All Rights Reserved.
+	LICENSE file should be included in distribution.
+OTHER
+HELP
+
+*/
+
+#include "unxsrad.h"
+
+//Global vars
+MYSQL gMysql;
+char gcQuery[8192]={""};
+char *gcBuildInfo="$Id: main.c 1969 2012-05-31 01:48:19Z Colin $";
+
+int main(int iArgc, char *cArgv[])
+{
+	return(0);
+}//main()
---
+++ trunk/unxsRAD/unxsrad.h
@@ -0,0 +1,21 @@
+/*
+FILE 
+	unxsrad.h
+	$Id$
+PURPOSE
+	unxsRAD job queue CLI standalone program header file
+LEGAL
+	(C) Gary Wallis 2012. All Rights Reserved.
+	LICENSE file should be included in distribution.
+OTHER
+HELP
+
+*/
+
+#include "mysqlrad.h"
+#include "local.h"
+
+//Global vars
+extern MYSQL gMysql;
+extern char gcQuery[];
+extern char *gcBuildInfo;
