28780493c61eabd19f4d2732fe6b685e4d51251c
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2012-09-27 22:46:10 +0000

    unxsSPS added some missing files.

---
+++ trunk/uSIPSwitch/unxsSPS/tclusterfunc.h
@@ -0,0 +1,397 @@
+/*
+FILE
+	$Id: modulefunc.h 2116 2012-09-19 23:00:28Z Gary $
+PURPOSE
+	Non schema-dependent table and application table related functions.
+AUTHOR
+	(C) 2001-2012 Gary Wallis for Unixservice, LLC.
+TEMPLATE VARS AND FUNCTIONS
+	ModuleFunctionProcess
+	ModuleFunctionProtos
+	cProject
+	cTableKey
+	cTableName
+*/
+
+
+
+void tClusterNavList(void);
+
+void ExtProcesstClusterVars(pentry entries[], int x)
+{
+	/*
+	register int i;
+	for(i=0;i<x;i++)
+	{
+	}
+	*/
+}//void ExtProcesstClusterVars(pentry entries[], int x)
+
+
+void ExttClusterCommands(pentry entries[], int x)
+{
+
+	if(!strcmp(gcFunction,"tClusterTools"))
+	{
+		
+		if(!strcmp(gcCommand,LANG_NB_NEW))
+                {
+			if(guPermLevel>=9)
+			{
+	                        ProcesstClusterVars(entries,x);
+                        	guMode=2000;
+	                        tCluster(LANG_NB_CONFIRMNEW);
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+                }
+		else if(!strcmp(gcCommand,LANG_NB_CONFIRMNEW))
+                {
+			if(guPermLevel>=9)
+			{
+				unsigned uContactParentCompany=0;
+                        	ProcesstClusterVars(entries,x);
+				GetClientOwner(guLoginClient,&uContactParentCompany);
+				
+                        	guMode=2000;
+				//Check entries here
+                        	guMode=0;
+
+				uCluster=0;
+#ifdef StandardFields
+				uCreatedBy=guLoginClient;
+				uOwner=uContactParentCompany;
+				uModBy=0;//Never modified
+				uModDate=0;//Never modified
+#endif
+				NewtCluster(0);
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+		}
+		else if(!strcmp(gcCommand,LANG_NB_DELETE))
+                {
+                        ProcesstClusterVars(entries,x);
+#ifdef StandardFields
+			if(uAllowDel(uOwner,uCreatedBy))
+#else
+			if(guPermLevel>=9)
+#endif
+			{
+	                        guMode=2001;
+				tCluster(LANG_NB_CONFIRMDEL);
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+                }
+                else if(!strcmp(gcCommand,LANG_NB_CONFIRMDEL))
+                {
+                        ProcesstClusterVars(entries,x);
+#ifdef StandardFields
+			if(uAllowDel(uOwner,uCreatedBy))
+#else
+			if(guPermLevel>=9)
+#endif
+			{
+				guMode=5;
+				DeletetCluster();
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+                }
+		else if(!strcmp(gcCommand,LANG_NB_MODIFY))
+                {
+                        ProcesstClusterVars(entries,x);
+#ifdef StandardFields
+			if(uAllowMod(uOwner,uCreatedBy))
+#else
+			if(guPermLevel>=9)
+#endif
+			{
+				guMode=2002;
+				tCluster(LANG_NB_CONFIRMMOD);
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+                }
+                else if(!strcmp(gcCommand,LANG_NB_CONFIRMMOD))
+                {
+                        ProcesstClusterVars(entries,x);
+#ifdef StandardFields
+			if(uAllowMod(uOwner,uCreatedBy))
+#else
+			if(guPermLevel>=9)
+#endif
+			{
+                        	guMode=2002;
+				//Check entries here
+                        	guMode=0;
+
+#ifdef StandardFields
+				uModBy=guLoginClient;
+#endif
+				ModtCluster();
+			}
+			else
+				tCluster("<blink>Error</blink>: Denied by permissions settings");
+                }
+	}
+
+}//void ExttClusterCommands(pentry entries[], int x)
+
+
+void ExttClusterButtons(void)
+{
+	OpenFieldSet("tCluster Aux Panel",100);
+	switch(guMode)
+        {
+                case 2000:
+			printf("<p><u>Enter/mod data</u><br>");
+                        printf(LANG_NBB_CONFIRMNEW);
+                break;
+
+                case 2001:
+                        printf("<p><u>Think twice</u><br>");
+                        printf(LANG_NBB_CONFIRMDEL);
+                break;
+
+                case 2002:
+			printf("<p><u>Review changes</u><br>");
+                        printf(LANG_NBB_CONFIRMMOD);
+                break;
+
+		default:
+			printf("<u>Table Tips</u><br>");
+			printf("<p><u>Record Context Info</u><br>");
+			printf("<p><u>Operations</u><br>");
+			//printf("<br><input type=submit class=largeButton title='Sample button help'"
+			//		" name=gcCommand value='Sample Button'>");
+			tClusterNavList();
+	}
+	CloseFieldSet();
+
+}//void ExttClusterButtons(void)
+
+
+void ExttClusterAuxTable(void)
+{
+
+}//void ExttClusterAuxTable(void)
+
+
+void ExttClusterGetHook(entry gentries[], int x)
+{
+	register int i;
+
+	for(i=0;i<x;i++)
+	{
+		if(!strcmp(gentries[i].name,"uCluster"))
+		{
+			sscanf(gentries[i].val,"%u",&uCluster);
+			guMode=6;
+		}
+	}
+	tCluster("");
+
+}//void ExttClusterGetHook(entry gentries[], int x)
+
+
+void ExttClusterSelect(void)
+{
+
+#ifdef StandardFields
+	unsigned uContactParentCompany=0;
+
+	GetClientOwner(guLoginClient,&uContactParentCompany);
+
+	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
+		sprintf(gcQuery,"SELECT %s FROM tCluster ORDER BY"
+				" uCluster",
+				VAR_LIST_tCluster);
+	else //If you own it, the company you work for owns the company that owns it,
+		//you created it, or your company owns it you can at least read access it
+		//select tTemplateSet.cLabel from tTemplateSet,tClient where tTemplateSet.uOwner=tClient.uClient and tClient.uOwner in (select uClient from tClient where uOwner=81 or uClient=51);
+	sprintf(gcQuery,"SELECT %s FROM tCluster,tClient WHERE tCluster.uOwner=tClient.uClient"
+				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)"
+				" ORDER BY uCluster",
+					VAR_LIST_tCluster,uContactParentCompany,uContactParentCompany);
+#else
+	sprintf(gcQuery,"SELECT %s FROM tCluster ORDER BY uCluster",VAR_LIST_tCluster);
+#endif
+					
+
+}//void ExttClusterSelect(void)
+
+
+void ExttClusterSelectRow(void)
+{
+#ifdef StandardFields
+	unsigned uContactParentCompany=0;
+
+	GetClientOwner(guLoginClient,&uContactParentCompany);
+
+	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
+                sprintf(gcQuery,"SELECT %s FROM tCluster WHERE uCluster=%u",
+			VAR_LIST_tCluster,uCluster);
+	else
+                sprintf(gcQuery,"SELECT %s FROM tCluster,tClient"
+                                " WHERE tCluster.uOwner=tClient.uClient"
+				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)"
+				" AND tCluster.uCluster=%u",
+                        		VAR_LIST_tCluster
+					,uContactParentCompany,uContactParentCompany
+					,uCluster);
+#else
+	sprintf(gcQuery,"SELECT %s FROM tCluster WHERE uCluster=%u",VAR_LIST_tCluster,uCluster);
+#endif
+
+}//void ExttClusterSelectRow(void)
+
+
+void ExttClusterListSelect(void)
+{
+	char cCat[512];
+#ifdef StandardFields
+	unsigned uContactParentCompany=0;
+	
+	GetClientOwner(guLoginClient,&uContactParentCompany);
+
+	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
+		sprintf(gcQuery,"SELECT %s FROM tCluster",
+				VAR_LIST_tCluster);
+	else
+		sprintf(gcQuery,"SELECT %s FROM tCluster,tClient"
+				" WHERE tCluster.uOwner=tClient.uClient"
+				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)",
+				VAR_LIST_tCluster
+				,uContactParentCompany
+				,uContactParentCompany);
+#else
+	sprintf(gcQuery,"SELECT %s FROM tCluster",VAR_LIST_tCluster);
+#endif
+
+	//Changes here must be reflected below in ExttClusterListFilter()
+        if(!strcmp(gcFilter,"uCluster"))
+        {
+                sscanf(gcCommand,"%u",&uCluster);
+		if(guPermLevel<10)
+			strcat(gcQuery," AND ");
+		else
+			strcat(gcQuery," WHERE ");
+		sprintf(cCat,"tCluster.uCluster=%u"
+						" ORDER BY uCluster",
+						uCluster);
+		strcat(gcQuery,cCat);
+        }
+        else if(1)
+        {
+                //None NO FILTER
+                strcpy(gcFilter,"None");
+		strcat(gcQuery," ORDER BY uCluster");
+        }
+
+}//void ExttClusterListSelect(void)
+
+
+void ExttClusterListFilter(void)
+{
+        //Filter
+        printf("&nbsp;&nbsp;&nbsp;Filter on ");
+        printf("<select name=gcFilter>");
+        if(strcmp(gcFilter,"uCluster"))
+                printf("<option>uCluster</option>");
+        else
+                printf("<option selected>uCluster</option>");
+        if(strcmp(gcFilter,"None"))
+                printf("<option>None</option>");
+        else
+                printf("<option selected>None</option>");
+        printf("</select>");
+
+}//void ExttClusterListFilter(void)
+
+
+void ExttClusterNavBar(void)
+{
+	printf(LANG_NBB_SKIPFIRST);
+	printf(LANG_NBB_SKIPBACK);
+	printf(LANG_NBB_SEARCH);
+
+	if(guPermLevel>=7 && !guListMode)
+		printf(LANG_NBB_NEW);
+
+#ifdef StandardFields
+	if(uAllowMod(uOwner,uCreatedBy))
+#else
+	if(guPermLevel>=9)
+#endif
+		printf(LANG_NBB_MODIFY);
+
+#ifdef StandardFields
+	if(uAllowDel(uOwner,uCreatedBy)) 
+#else
+	if(guPermLevel>=9)
+#endif
+		printf(LANG_NBB_DELETE);
+
+#ifdef StandardFields
+	if(uOwner)
+#else
+	if(guPermLevel>=9)
+#endif
+		printf(LANG_NBB_LIST);
+
+	printf(LANG_NBB_SKIPNEXT);
+	printf(LANG_NBB_SKIPLAST);
+	printf("&nbsp;&nbsp;&nbsp;\n");
+
+}//void ExttClusterNavBar(void)
+
+
+void tClusterNavList(void)
+{
+        MYSQL_RES *res;
+        MYSQL_ROW field;
+	unsigned uContactParentCompany=0;
+
+	GetClientOwner(guLoginClient,&uContactParentCompany);
+	GetClientOwner(uContactParentCompany,&guReseller);//Get owner of your owner...
+	if(guReseller==1) guReseller=0;//...except Root companies
+	
+#ifdef StandardFields
+	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
+		sprintf(gcQuery,"SELECT uCluster,cLabel FROM tCluster ORDER BY cLabel");
+	else
+		sprintf(gcQuery,"SELECT tCluster.uCluster,"
+				" tCluster.cLabel"
+				" FROM tCluster,tClient"
+				" WHERE tCluster.uOwner=tClient.uClient"
+				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)",
+				uContactParentCompany
+				,uContactParentCompany);
+#else
+	sprintf(gcQuery,"SELECT uCluster,cLabel FROM tCluster ORDER BY cLabel");
+#endif
+        mysql_query(&gMysql,gcQuery);
+        if(mysql_errno(&gMysql))
+        {
+        	printf("<p><u>tClusterNavList</u><br>\n");
+                printf("%s",mysql_error(&gMysql));
+                return;
+        }
+
+        res=mysql_store_result(&gMysql);
+	if(mysql_num_rows(res))
+	{	
+        	printf("<p><u>tClusterNavList</u><br>\n");
+
+	        while((field=mysql_fetch_row(res)))
+			printf("<a class=darkLink href=unxsSPS.cgi?gcFunction=tCluster"
+				"&uCluster=%s>%s</a><br>\n",
+				field[0],field[1]);
+	}
+        mysql_free_result(res);
+
+}//void tClusterNavList(void)
+
+
