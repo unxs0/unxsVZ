d99dd7919385c8a41c001049862bd221c2ab1410
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2012-10-10 01:55:41 +0000

    unxsSPS work continues getting close to an alpha test system.

--- trunk/uSIPSwitch/unxsSPS/tdidfunc.h
+++ trunk/uSIPSwitch/unxsSPS/tdidfunc.h
@@ -15,7 +15,6 @@ TEMPLATE VARS AND FUNCTIONS
 
 
 
-void tDIDNavList(void);
 
 void ExtProcesstDIDVars(pentry entries[], int x)
 {
@@ -166,7 +165,6 @@ void ExttDIDButtons(void)
 			printf("<p><u>Operations</u><br>");
 			//printf("<br><input type=submit class=largeButton title='Sample button help'"
 			//		" name=gcCommand value='Sample Button'>");
-			tDIDNavList();
 	}
 	CloseFieldSet();
 
@@ -251,10 +249,6 @@ void ExttDIDSelectRow(void)
 void ExttDIDListSelect(void)
 {
 	char cCat[512];
-#ifdef StandardFields
-	unsigned uContactParentCompany=0;
-	
-	GetClientOwner(guLoginClient,&uContactParentCompany);
 
 	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
 		sprintf(gcQuery,"SELECT %s FROM tDID",
@@ -264,11 +258,8 @@ void ExttDIDListSelect(void)
 				" WHERE tDID.uOwner=tClient.uClient"
 				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)",
 				VAR_LIST_tDID
-				,uContactParentCompany
-				,uContactParentCompany);
-#else
-	sprintf(gcQuery,"SELECT %s FROM tDID",VAR_LIST_tDID);
-#endif
+				,guCompany
+				,guCompany);
 
 	//Changes here must be reflected below in ExttDIDListFilter()
         if(!strcmp(gcFilter,"uDID"))
@@ -278,11 +269,18 @@ void ExttDIDListSelect(void)
 			strcat(gcQuery," AND ");
 		else
 			strcat(gcQuery," WHERE ");
-		sprintf(cCat,"tDID.uDID=%u"
-						" ORDER BY uDID",
-						uDID);
+		sprintf(cCat,"tDID.uDID=%u ORDER BY uDID",uDID);
 		strcat(gcQuery,cCat);
         }
+        if(!strcmp(gcFilter,"cDID"))
+        {
+		if(guPermLevel<10)
+			strcat(gcQuery," AND ");
+		else
+			strcat(gcQuery," WHERE ");
+		sprintf(cCat,"tDID.cDID LIKE '%s%%' ORDER BY cDID",gcCommand);
+		strcat(gcQuery,cCat);
+	}
         else if(1)
         {
                 //None NO FILTER
@@ -302,6 +300,10 @@ void ExttDIDListFilter(void)
                 printf("<option>uDID</option>");
         else
                 printf("<option selected>uDID</option>");
+        if(strcmp(gcFilter,"cDID"))
+                printf("<option>cDID</option>");
+        else
+                printf("<option selected>cDID</option>");
         if(strcmp(gcFilter,"None"))
                 printf("<option>None</option>");
         else
@@ -320,25 +322,13 @@ void ExttDIDNavBar(void)
 	if(guPermLevel>=7 && !guListMode)
 		printf(LANG_NBB_NEW);
 
-#ifdef StandardFields
 	if(uAllowMod(uOwner,uCreatedBy))
-#else
-	if(guPermLevel>=9)
-#endif
 		printf(LANG_NBB_MODIFY);
 
-#ifdef StandardFields
 	if(uAllowDel(uOwner,uCreatedBy)) 
-#else
-	if(guPermLevel>=9)
-#endif
 		printf(LANG_NBB_DELETE);
 
-#ifdef StandardFields
 	if(uOwner)
-#else
-	if(guPermLevel>=9)
-#endif
 		printf(LANG_NBB_LIST);
 
 	printf(LANG_NBB_SKIPNEXT);
@@ -346,52 +336,5 @@ void ExttDIDNavBar(void)
 	printf("&nbsp;&nbsp;&nbsp;\n");
 
 }//void ExttDIDNavBar(void)
-
-
-void tDIDNavList(void)
-{
-        MYSQL_RES *res;
-        MYSQL_ROW field;
-	unsigned uContactParentCompany=0;
-
-	GetClientOwner(guLoginClient,&uContactParentCompany);
-	GetClientOwner(uContactParentCompany,&guReseller);//Get owner of your owner...
-	if(guReseller==1) guReseller=0;//...except Root companies
-	
-#ifdef StandardFields
-	if(guLoginClient==1 && guPermLevel>11)//Root can read access all
-		sprintf(gcQuery,"SELECT uDID,cLabel FROM tDID ORDER BY cLabel");
-	else
-		sprintf(gcQuery,"SELECT tDID.uDID,"
-				" tDID.cLabel"
-				" FROM tDID,tClient"
-				" WHERE tDID.uOwner=tClient.uClient"
-				" AND tClient.uOwner IN (SELECT uClient FROM tClient WHERE uOwner=%u OR uClient=%u)",
-				uContactParentCompany
-				,uContactParentCompany);
-#else
-	sprintf(gcQuery,"SELECT uDID,cLabel FROM tDID ORDER BY cLabel");
-#endif
-        mysql_query(&gMysql,gcQuery);
-        if(mysql_errno(&gMysql))
-        {
-        	printf("<p><u>tDIDNavList</u><br>\n");
-                printf("%s",mysql_error(&gMysql));
-                return;
-        }
-
-        res=mysql_store_result(&gMysql);
-	if(mysql_num_rows(res))
-	{	
-        	printf("<p><u>tDIDNavList</u><br>\n");
-
-	        while((field=mysql_fetch_row(res)))
-			printf("<a class=darkLink href=unxsSPS.cgi?gcFunction=tDID"
-				"&uDID=%s>%s</a><br>\n",
-				field[0],field[1]);
-	}
-        mysql_free_result(res);
-
-}//void tDIDNavList(void)
 
 
