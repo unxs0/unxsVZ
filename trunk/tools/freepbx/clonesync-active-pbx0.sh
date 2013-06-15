226e9e57fe32f4ad4e248a120f27e80a6732b7e0
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2013-06-15 15:49:45 +0000

    unxsVZ working beta clonesync-active-pbx0.sh for FreePBX OBX containers.

---
+++ trunk/tools/freepbx/clonesync-active-pbx0.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+#
+#FILE
+#	clonesync-remote.sh
+#PURPOSE
+#	keep a cloned live pbx container
+#NOTES
+#	This is an asterisk/freepbx specific sync script.
+#	Example script for use with unxsVZ virtualization
+#	manager.
+#LEGAL
+#	Copyright 2013 Gary Wallis and Ricardo Armas for Unixservice, LLC.
+#	GPLv2 license applies.
+
+fLog() { echo "`date +%b' '%d' '%T` $0[$$]: $@"; }
+echo "`date +%b' '%d' '%T` $0[$$]: $@" >> /tmp/clonesync-active.log 2>&1
+
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ];then
+	echo "usage: $0 <source VEID> <target VEID> <target node host>";
+	exit 0;
+fi
+uContainer=$1;
+uRemoteContainer=$2;
+cTargetNode=$3;
+cLocalNetFileSpec="/etc/unxsvz/asterisk/localnet";
+cExternIpFileSpec="/etc/unxsvz/asterisk/externip";
+
+/usr/sbin/vzlist | grep $uContainer  > /dev/null 2>&1;
+if [ $? != 0 ];then
+        fLog "$uContainer not running. Will not sync.";
+	#exit code 7 is temporary failure. Should be used by unxsVZ jobqueue.c to
+	#maek sure backup date is NOT updated!
+        exit 7;
+fi
+
+uActiveChannels=`/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer  \"asterisk -rx 'sip show channelstats'\" | grep active | cut -f1 -d ' '"`
+if [ $uActiveChannels != 0 ];then
+        fLog "$uContainer has active calls. Will not sync.";
+        exit 7;
+fi
+
+#do not run if another (same source and target VEIDs) clone job is also running
+cContainerLock="/tmp/clonesync.sh.lock.$1.$2";
+if [ -d $cContainerLock ]; then
+	fLog "waiting for lock release $cContainerLock";
+	exit 1;
+else
+	mkdir $cContainerLock; 
+fi
+
+
+#make sure ssh is working
+#change /etc/ssh/ssh_conf for non standard port
+/usr/bin/ssh $cTargetNode "ls /vz/private/$uRemoteContainer > /dev/null 2>&1";
+if [ $? != 0 ];then
+	fLog "/usr/bin/ssh $cTargetNode ls /vz/private/$uRemoteContainer failed";
+	#rollback
+	rmdir $cContainerLock;
+	exit 2;
+fi
+
+cAMPDBUSER=`/usr/sbin/vzctl exec2 $uContainer '/bin/grep "^AMPDBUSER=" /etc/amportal.conf | tail -n 1 | /bin/cut -f 2 -d ='`;
+cAMPDBPASS=`/usr/sbin/vzctl exec2 $uContainer '/bin/grep "^AMPDBPASS=" /etc/amportal.conf | tail -n 1 | /bin/cut -f 2 -d ='`;
+#functions
+funcQuery() { 
+
+	/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec $uRemoteContainer \" echo '$@' | mysql -u $cAMPDBUSER -p$cAMPDBPASS asterisk \" >/dev/null 2>&1";
+}
+
+#dump source sql
+/usr/sbin/vzctl exec2 $uContainer "/usr/bin/mysqldump -u $cAMPDBUSER --password=$cAMPDBPASS asterisk > /tmp/dump.sql";
+#copy dump to target
+/usr/bin/scp /vz/private/$uContainer/tmp/dump.sql $cTargetNode:/vz/private/$uRemoteContainer/tmp/dump.sql \
+		2>> /tmp/clonesync-active.log 1> /dev/null
+
+#restore on target
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"cat /tmp/dump.sql | mysql -u $cAMPDBUSER --password=$cAMPDBPASS asterisk\";" \
+		2>> /tmp/clonesync-active.log 1> /dev/null
+
+#
+#localnet setup
+cLocalNet=`/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec $uRemoteContainer cat '$cLocalNetFileSpec'"`;
+cSQL="INSERT INTO sipsettings SET keyword='\''localnet'\'',data='\''$cLocalNet'\'',seq=6,type=9";
+funcQuery $cSQL;
+if [ $? != 0 ];then
+	cSQL="UPDATE sipsettings SET data='\''$cLocalNet'\'',seq=6,type=9 WHERE keyword='\''localnet'\''";
+	funcQuery $cSQL;
+	if [ $? != 0 ];then
+		echo "insert and update failed";
+		exit 1;
+	fi
+fi
+#localnet setup end
+#
+
+#
+#externip setup
+cExternIp=`/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec $uRemoteContainer cat '$cExternIpFileSpec'"`;
+cSQL="INSERT INTO sipsettings SET keyword='\''externip'\'',data='\''$cExternIp'\'',seq=7,type=9";
+funcQuery $cSQL;
+if [ $? != 0 ];then
+        cSQL="UPDATE sipsettings SET data='\''$cExternIp'\'',seq=7,type=9 WHERE keyword='\''externip'\''";
+        funcQuery $cSQL;
+        if [ $? != 0 ];then
+                echo "insert and update failed";
+                exit 1;
+        fi
+fi
+#externip setup end
+#
+
+#only log errors
+#retrieve conf
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"/var/lib/asterisk/bin/retrieve_conf\""\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#stop asterisk and freepbx
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"amportal stop\""\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#sync etc/asterisk
+/usr/bin/rsync -axlH  --rsh '/usr/bin/ssh -ax -c arcfour' --delete /vz/private/$1/etc/asterisk/ $3:/vz/private/$2/etc/asterisk/\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#sync spool
+/usr/bin/rsync -axlH  --rsh '/usr/bin/ssh -ax -c arcfour' --delete /vz/private/$1/var/spool/asterisk/ $3:/vz/private/$2/var/spool/asterisk\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#sync www
+/usr/bin/rsync -axlH  --rsh '/usr/bin/ssh -ax -c arcfour' --delete /vz/private/$1/var/www/ $3:/vz/private/$2/var/www/\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#sync var/lib/asterisk
+/usr/bin/rsync -axlH  --rsh '/usr/bin/ssh -ax -c arcfour' --delete /vz/private/$1/var/lib/asterisk/ $3:/vz/private/$2/var/lib/asterisk/\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#restore rtp.conf 
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"cp /etc/unxsvz/asterisk/rtp.conf  /etc/asterisk/rtp.conf\""\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#start asterisk and freepbx again
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"amportal start\""\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+#reload config
+/usr/bin/ssh $cTargetNode "/usr/sbin/vzctl exec2 $uRemoteContainer \"/var/lib/asterisk/bin/module_admin reload\""\
+		2>> /tmp/clonesync-active.log 1> /dev/null
+
+#everything ok clean exit
+rmdir $cContainerLock;
+exit $uExitVal;
