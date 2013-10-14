f4315e282a848049cb945f41d97c342968fa1f0b
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2013-10-14 22:51:11 +0000

    unxsVZ beta tested script commit.

--- trunk/tools/datacenter/InstallNewZabbixInPBXs.sh
+++ trunk/tools/datacenter/InstallNewZabbixInPBXs.sh
@@ -8,11 +8,16 @@
 #      (C) 2011,2013 Gary Wallis for Unixservice, LLC.
 #      GPLv3 license applies see root dir LICENSE
 
-#use this to run only the json api
-#cOnlySetProxy="true";
+#
+#this file contains confidential info keep secure!
+cNewRange="6.4.5.0";
+cRefRange="19.20.11.0";
+cDomain="isp.net";
+cMySQLConnect="/usr/bin/mysql -h server -u unxsvz -pwsxedc unxsvz";
+#
 
-if [ "$1" == "" ] || ( [ "$1" != "install" ] && [ "$1" != "help" ] );then
-	echo "usage: $0 <install|help> [<veid>]";
+if [ "$1" == "" ] || ( [ "$1" != "install" ] && [ "$1" != "help" ] && [ "$1" != "setproxy" ] );then
+	echo "usage: $0 <install|setproxy|help> [<veid>]";
 	exit 0;
 fi
 
@@ -27,6 +32,11 @@ if [ ! -d "/root/zabbix_agentd_upgrade" ];then
 	exit 1;
 fi
 
+#use this to run only the json api
+cOnlySetProxy="false";
+if [ "$1" == "setproxy" ];then
+	cOnlySetProxy="true";
+fi
 
 cGroup="Production PBXs";
 
@@ -35,7 +45,7 @@ if [ $? != 0 ];then
 	echo "cNode hostname error";
 	exit 2;
 fi
-uNode=`echo "SELECT uNode FROM tNode WHERE cLabel='$cNode'"|/usr/bin/mysql -h rc1 -u unxsvz -pwsxedc unxsvz|grep -v uNode`;
+uNode=`echo "SELECT uNode FROM tNode WHERE cLabel='$cNode'"|$cMySQLConnect|grep -v uNode`;
 if [ $? != 0 ];then
 	echo "uNode SELECT error";
 	exit 3;
@@ -45,8 +55,8 @@ if [ "$uNode" == "" ];then
 	exit 4;
 fi
 
-#stopped or running containers
-for uContainer in `echo "SELECT tContainer.uContainer FROM tContainer,tGroup,tGroupGlue WHERE tGroup.cLabel='$cGroup' AND tGroup.uGroup=tGroupGlue.uGroup AND tContainer.uContainer=tGroupGlue.uContainer AND tContainer.uNode=$uNode AND (tContainer.uStatus=1 OR tContainer.uStatus=31)" | /usr/bin/mysql -h rc1 -u unxsvz -pwsxedc unxsvz | grep -v uContainer`;do
+#running containers only
+for uContainer in `echo "SELECT tContainer.uContainer FROM tContainer,tGroup,tGroupGlue WHERE tGroup.cLabel='$cGroup' AND tGroup.uGroup=tGroupGlue.uGroup AND tContainer.uContainer=tGroupGlue.uContainer AND tContainer.uNode=$uNode AND tContainer.uStatus=1" | $cMySQLConnect | grep -v uContainer`;do
 
 	#run for only one option
 	if [ "$2" != "" ] && [ "$uContainer" != "$2" ];then
@@ -82,18 +92,27 @@ for uContainer in `echo "SELECT tContainer.uContainer FROM tContainer,tGroup,tGr
 	fi
 
 	#update /etc/zabbix/ files only once please
-	sed -i -e 's/Server=zabbix.isp.net$/Server=zabbix.isp.net,zabbix-proxy0.isp.net/g' \
+	sed -i -e "s/Server=zabbix.$cDomain\$/Server=zabbix.$cDomain,zabbix-proxy0.$cDomain/g" \
 			/vz/private/$uContainer/etc/zabbix/zabbix_agentd.conf;
 
 	#update iptables. add new subnet for ping only once please
-	grep -w "6.4.5.0" /vz/private/$uContainer/etc/sysconfig/iptables > /dev/null;
+	grep -w "$cNewRange" /vz/private/$uContainer/etc/sysconfig/iptables > /dev/null;
 	if [ "$?" != "0" ];then
-		sed -i -e 's/-A INPUT -p icmp -m icmp -s 19.20.11.0\/24 -j ACCEPT/-A INPUT -p icmp -m icmp -s 19.20.11.0\/24 -j ACCEPT\n-A INPUT -p icmp -m icmp -s 6.4.5.0\/24 -j ACCEPT/g' \
+		sed -i -e "s/-A INPUT -p icmp -m icmp -s $cRefRange\/24 -j ACCEPT/-A INPUT -p icmp -m icmp -s $cRefRange\/24 -j ACCEPT\n-A INPUT -p icmp -m icmp -s $cNewRange\/24 -j ACCEPT/g" \
 			/vz/private/$uContainer/etc/sysconfig/iptables;
 	fi
 
 	#restart zabbix-agent /tmp/zabbix_agentd.pid
-	/usr/sbin/vzctl exec $uContainer 'kill `cat /tmp/zabbix_agentd.pid`;sleep 1';
+	/usr/sbin/vzctl exec2 $uContainer 'kill `cat /tmp/zabbix_agentd.pid`';
+	if [ "$?" != "0" ];then
+		echo "could not kill zabbix_agentd w/pid file, trying killall";
+		sleep 1;
+		/usr/sbin/vzctl exec2 $uContainer 'killall zabbix_agentd';
+		if [ "$?" != "0" ];then
+			echo "could not kill or killall zabbix_agentd";
+		fi
+	fi;
+	sleep 1;
 	/usr/sbin/vzctl exec $uContainer "service zabbix_agentd start";
 
 	#restart iptables
@@ -101,9 +120,10 @@ for uContainer in `echo "SELECT tContainer.uContainer FROM tContainer,tGroup,tGr
 
   else
 
-	#now you need to change zabbix to use proxy
+	#only doing setproxy below. provide some feedback.
 	echo $cHostname;
-	/usr/sbin/UpdateZabbixSetProxy.sh $cHostname 14124;
   fi
+	#now you need to change zabbix to use proxy
+	/usr/sbin/UpdateZabbixSetProxy.sh $cHostname 14124 > /dev/null;
 
 done
