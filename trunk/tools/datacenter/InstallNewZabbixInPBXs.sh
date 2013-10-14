8ff25900172a383e42aecfa9237308ce93b6a3f1
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2013-10-14 15:12:09 +0000

    unxsVZ added new alpha script for updating zabbix_agentd in existing PBXs.

---
+++ trunk/tools/datacenter/InstallNewZabbixInPBXs.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+if [ "$1" == "" ] || ( [ "$1" != "install" ] && [ "$1" != "help" ] );then
+	echo "usage: $0 <install|help> [<veid>]";
+	exit 0;
+fi
+
+if [ "$1" == "help" ];then
+	echo "This script upgrades current PBX zabbix_agentd to the version in the /root/zabbix_agentd_upgrade dir";
+	cat /root/zabbix_agentd_upgrade/README;
+	exit 0;
+fi
+
+if [ ! -d "/root/zabbix_agentd_upgrade" ];then
+	echo "No /root/zabbix_agentd_upgrade directory. Aborting.";
+	exit 1;
+fi
+
+
+cGroup="Production PBXs";
+
+cNode=`hostname -s`;
+if [ $? != 0 ];then
+	echo "cNode hostname error";
+	exit 2;
+fi
+uNode=`echo "SELECT uNode FROM tNode WHERE cLabel='$cNode'"|/usr/bin/mysql -h rc1 -u unxsvz -pwsxedc unxsvz|grep -v uNode`;
+if [ $? != 0 ];then
+	echo "uNode SELECT error";
+	exit 3;
+fi
+if [ "$uNode" == "" ];then
+	echo "uNode could not be determined for $cNode";
+	exit 4;
+fi
+
+#stopped or running containers
+for uContainer in `echo "SELECT tContainer.uContainer FROM tContainer,tGroup,tGroupGlue WHERE tGroup.cLabel='$cGroup' AND tGroup.uGroup=tGroupGlue.uGroup AND tContainer.uContainer=tGroupGlue.uContainer AND tContainer.uNode=$uNode AND (tContainer.uStatus=1 OR tContainer.uStatus=31)" | /usr/bin/mysql -h rc1 -u unxsvz -pwsxedc unxsvz | grep -v uContainer`;do
+
+	#run for only one option
+	if [ "$2" != "" ] && [ "$uContainer" != "$2" ];then
+		continue;
+	fi
+
+	#check local system
+	/usr/sbin/vzlist $uContainer;
+	if [ "$?" != "0" ];then
+		echo "vzlist failed for $uContainer. next.";
+		continue;
+	fi
+
+	#install/update binary and provide backwords compatibility sym link
+	if [ -f /root/zabbix_agentd_upgrade/zabbix_agentd ];then
+		cp /root/zabbix_agentd_upgrade/zabbix_agentd /vz/private/$uContainer/usr/sbin/zabbix_agentd; 
+		if [ "$?" != "0" ];then
+			echo "cp failed $uContainer.";
+			continue;
+		fi
+		if [ ! -f /vz/private/$uContainer/usr/local/sbin/zabbix_agentd.old ];then
+			mv -i /vz/private/$uContainer/usr/local/sbin/zabbix_agentd /vz/private/$uContainer/usr/local/sbin/zabbix_agentd.old;
+			cd /vz/private/$uContainer/usr/local/sbin/;
+			ln -s /usr/sbin/zabbix_agentd zabbix_agentd;
+			if [ "$?" != "0" ];then
+				echo "ln -s failed";
+			fi
+		fi
+	fi
+
+	#update /etc/zabbix/ files only once please
+	sed -i -e 's/Server=zabbix.callingcloud.net$/Server=zabbix.callingcloud.net,zabbix-proxy0.callingcloud.net/g' \
+			/vz/private/$uContainer/etc/zabbix/zabbix_agentd.conf;
+
+	#update iptables. add new subnet for ping only once please
+	grep -w "65.49.53.0" /vz/private/$uContainer/etc/sysconfig/iptables > /dev/null;
+	if [ "$?" != "0" ];then
+		sed -i -e 's/-A INPUT -p icmp -m icmp -s 199.200.101.0\/24 -j ACCEPT/-A INPUT -p icmp -m icmp -s 199.200.101.0\/24 -j ACCEPT\n-A INPUT -p icmp -m icmp -s 65.49.53.0\/24 -j ACCEPT/g' \
+			/vz/private/$uContainer/etc/sysconfig/iptables;
+	fi
+
+	#restart zabbix-agent /tmp/zabbix_agentd.pid
+	/usr/sbin/vzctl exec $uContainer "kill `cat /tmp/zabbix_agentd.pid`;sleep 1";
+	/usr/sbin/vzctl exec $uContainer "service zabbix_agentd start";
+
+	#restart iptables
+	/usr/sbin/vzctl exec $uContainer "service iptables restart";
+
+	#now you need to change zabbix to use proxy
+done
