5986fcbdd484cde34e413cb697850ef3d3fca299
Dylan <Dylan@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2014-01-27 00:53:22 +0000

    unxsVZ always deploy -clone containers stop. uAWAITCREATE wierdness.

--- trunk/tcontainerfunc.h
+++ trunk/tcontainerfunc.h
@@ -5539,7 +5539,7 @@ while((field=mysql_fetch_row(res)))
 					InitContainerProps(&sContainer);
 					GetContainerProps(uCtContainer,&sContainer);
 					if( (sContainer.uStatus==uSTOPPED || sContainer.uStatus==uAWAITDEL || 
-							sContainer.uStatus==uAWAITACT || sContainer.uStatus==uAWAITCREATE )
+							sContainer.uStatus==uAWAITACT )
 						&& (sContainer.uOwner==guCompany || guCompany==1))
 					{
 							SetContainerStatus(uCtContainer,11);
