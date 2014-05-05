2814d66bfbba99815851a44baf11c68fa2d0254b
Gary <ggw@unixservice.com>
2014-05-05 22:41:56 +0000

    unxsVZ snort barnyard agent beta version.

---
+++ agents/snort/sql/CreateAndImportGeoIPCountry.sql
@@ -0,0 +1,37 @@
+USE snort;
+
+DROP TABLE IF EXISTS tGeoIPFromCSV;
+CREATE TABLE tGeoIPFromCSV (
+        cStartIP CHAR(15) NOT NULL,
+        cEndIP CHAR(15) NOT NULL,
+        uStartIP INT UNSIGNED NOT NULL,
+        uEndIP INT UNSIGNED NOT NULL,
+        cCountryCode CHAR(2) NOT NULL,
+        cCountryName VARCHAR(50) NOT NULL
+        );
+
+DROP TABLE IF EXISTS tGeoIPCountryCode;
+CREATE TABLE IF NOT EXISTS tGeoIPCountryCode (
+        uGeoIPCountryCode TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        cCountryCode CHAR(2) NOT NULL,
+        cCountryName VARCHAR(50) NOT NULL
+        );
+
+DROP TABLE IF EXISTS tGeoIP;
+CREATE TABLE tGeoIP (
+        uStartIP INT UNSIGNED NOT NULL,
+        uEndIP INT UNSIGNED NOT NULL, UNIQUE (uEndIP),
+        uGeoIPCountryCode TINYINT UNSIGNED NOT NULL
+        );
+
+LOAD DATA INFILE '/home/unxs/GeoIPCountryWhois.csv' 
+        INTO TABLE tGeoIPFromCSV
+        FIELDS TERMINATED BY ',' ENCLOSED BY '"' 
+        LINES TERMINATED BY '\n' STARTING BY '' 
+        ;
+
+INSERT INTO tGeoIPCountryCode SELECT DISTINCT NULL,cCountryCode,cCountryName FROM tGeoIPFromCSV;
+
+INSERT INTO tGeoIP SELECT uStartIP,uEndIP,uGeoIPCountryCode FROM tGeoIPFromCSV NATURAL JOIN tGeoIPCountryCode;
+
+DROP TABLE IF EXISTS tGeoIPFromCSV;
