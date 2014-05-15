fe00fee7accb9bf2d764dbaaf0d87c4c2bdccdb7
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2014-05-15 20:26:07 +0000

    unxsVZ snort agent install notes corrected.

--- trunk/agents/snort/scripts/CreateCentOS-Snort-Barnyard.tgz.sh
+++ trunk/agents/snort/scripts/CreateCentOS-Snort-Barnyard.tgz.sh
@@ -9,6 +9,7 @@
 		/etc/rc.d/init.d/barnyard2\
 		/etc/snort/barnyard.conf\
 		/usr/bin/barnyard2\
+		/usr/local/bin/barnyard2\
 		/var/log/snort/eth0/archive\
 		/var/log/snort/eth0/barnyard2.waldo\
 		/etc/sysconfig/snort\
@@ -16,12 +17,28 @@
 		/etc/logrotate.d/snort\
 		/etc/cron.d/unxsvz_snort.cron\
 		/etc/snort\
-		/usr/share/man/man8/snort_selinux.8.gz\
 		/usr/share/man/man8/snort.8.gz\
 		/usr/bin/snort_control\
 		/usr/lib64/snort-2.9.6.1_dynamicengine\
 		/usr/lib64/snort-2.9.6.1_dynamicpreprocessor\
+		/usr/lib64/libsfbpf*\
 		/usr/sbin/snort\
 		/usr/sbin/snort-plain\
+		/usr/sbin/unxsSnortPost*.sh\
 		/usr/local/lib/snort_dynamicpreprocessor\
 		/usr/lib64/libdnet*
+
+#after installing these files:
+#	coment off /etc/cron.d/unxsvz_snort.cron temporarily 4 test cli
+#	comment off /usr/sbin/unxsSnortPost*.sh so you wont send email while testing.
+#	setup mysql see /etc/snort/install
+#	svn co unxsSnort and compile and test
+#	make sure that /etc/snort/ignore.bpf has the correct ranges in it.
+#	groupadd snort
+#	useradd snort
+#	chown -R snort:snort /var/log/snort
+#	service snortd start
+#	service barnyard2 start
+#	look at log files
+#	make sure data is being written to mySQL snort db
+#	run unxsSnort --process from the CLI
