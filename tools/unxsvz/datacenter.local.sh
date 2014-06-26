38ce722e2d48d9f2c242007fff061b76cb72959a
Gary <ggw@unixservice.com>
2014-06-26 16:32:54 +0000

    unxsVZ some datacenter script updates.

--- tools/unxsvz/datacenter.local.sh
+++ tools/unxsvz/datacenter.local.sh
@@ -13,20 +13,26 @@
 #NOTES
 #	This file contains confidential info keep secure!
 #	Set chmod 500
+#
+#	The robustness -mmin -5 code below should also be implemented in the seed first two items.
 
 
 DATACENTER="YourDatacenterLabel";
 
 #aggreagation instructions generation
-DEFSECTION="DEF:node13-in=/var/lib/rrd/node13.rrd:in:MAX DEF:node13-out=/var/lib/rrd/node13.rrd:out:MAX DEF:node14-in=/var/lib/rrd/node14.rrd:in:MAX DEF:node14-out=/var/lib/rrd/node14.rrd:out:MAX";
-CDEFSECTIONIN="CDEF:in=node13-in,node14-in,+";
-CDEFSECTIONOUT="CDEF:out=node13-out,node14-out,+";
+DEFSECTION="DEF:node1-in=/var/lib/rrd/node1.rrd:in:MAX DEF:node1-out=/var/lib/rrd/node1.rrd:out:MAX DEF:node2-in=/var/lib/rrd/node2.rrd:in:MAX DEF:node2-out=/var/lib/rrd/node2.rrd:out:MAX";
+CDEFSECTIONIN="CDEF:in=node1-in,node2-in,+";
+CDEFSECTIONOUT="CDEF:out=node1-out,node2-out,+";
 
 #note how we start 2 after
-for N in $(seq 1 12 ); do
-	DEFSECTION="$DEFSECTION DEF:node$N-in=/var/lib/rrd/node$N.rrd:in:MAX DEF:node$N-out=/var/lib/rrd/node$N.rrd:out:MAX";
-	CDEFSECTIONIN="$CDEFSECTIONIN,node$N-in,+";
-	CDEFSECTIONOUT="$CDEFSECTIONOUT,node$N-out,+";
+for N in $(seq 3 12 ); do
+	cFile="";
+	cFile=`find /var/lib/rrd/rc$N.rrd -mmin -5`;
+	if [ ! "$cFile" == "" ];then
+		DEFSECTION="$DEFSECTION DEF:node$N-in=/var/lib/rrd/node$N.rrd:in:MAX DEF:node$N-out=/var/lib/rrd/node$N.rrd:out:MAX";
+		CDEFSECTIONIN="$CDEFSECTIONIN,node$N-in,+";
+		CDEFSECTIONOUT="$CDEFSECTIONOUT,node$N-out,+";
+	fi
 done;
 
 #debug only
