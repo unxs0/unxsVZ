112d9035e12e6295bf0f75a5283c90ced5956644
Gary <Gary@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2014-10-30 14:46:33 +0000

    Limit email to 100 lines.

--- trunk/agents/snort/scripts/unxsSnortPostAddJob.sh
+++ trunk/agents/snort/scripts/unxsSnortPostAddJob.sh
@@ -7,7 +7,7 @@ SUBJECT="IDS proposes that $1 be blocked";
 echo "$1 $2" >> /tmp/unxsSnortPostAddJob.sh.log;
 
 if [ -f "$2" ] && [ -s "$2" ];then
-	cat $2 | /bin/mail -s "$SUBJECT" -b "$BCC_ADDRESS" -r "$FROM_ADDRESS" $TO_ADDRESS;
+	/usr/bin/head -n 100 $2 | /bin/mail -s "$SUBJECT" -b "$BCC_ADDRESS" -r "$FROM_ADDRESS" $TO_ADDRESS;
 else
 	BODY="IDS proposes that $1 be blocked";
 	echo "$BODY" | /bin/mail -s "$SUBJECT" -b "$BCC_ADDRESS" -r "$FROM_ADDRESS" $TO_ADDRESS;
