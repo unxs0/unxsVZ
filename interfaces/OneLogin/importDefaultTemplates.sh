77e6fe208d4965e0dc4549d3c221e078861cd6a9
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2015-01-05 20:47:00 +0000

    unxsVZ refactoring work flow for template developement that allows local modified templates to safely coexist.

---
+++ interfaces/OneLogin/importDefaultTemplates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#FILE
+# $Id: importDefaultTemplates.sh 1335 2010-04-09 16:05:21Z Gary $
+#PURPOSE
+#	Import all OneLogin.cgi templates in one fell swoop.
+#AUTHOR/LEGAL
+#	(C) 2010,2015 Gary Wallis for Unixservice, LLC.
+#	GPLv2 license applies. See included LICENSE file in source root dir.
+#
+if [ ! -z "$CGIDIR" ]; then
+    if [ -e "${CGIDIR}unxsVZ.cgi" ]; then
+    for cFile in `find ./templates.default -maxdepth 1 -type f`; do
+    	if [[ $cFile != *.swp ]] && [[ $cFile != *.default ]];then
+        	${CGIDIR}unxsVZ.cgi ImportTemplateFile  `echo $cFile | cut -f 3 -d /`  ./$cFile plain "OneLogin Interface";
+	fi
+    done
+    else
+    echo "unxsVZ.cgi isn't present in the CGIDIR you've defined!";
+    fi
+else
+    echo "You haven't defined a CGIDIR, export CGIDIR=/cgi-bin/";
+fi
