ed3c30d24883c9ada80149aa5cc8da3d138cb514
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2015-04-15 13:53:06 +0000

    unxsScripts added EveryNode scripts.

---
+++ unxsScripts/unxsvz/sbin/unxsvzEveryNodeScp.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+#FILE
+#	/usr/sbin/unxsvzEveryNodeScp.sh
+#PURPOSE
+#	scp a file to all OTHER active nodes in the
+#	same datacenter
+#AUTHOR/LEGAL
+#	(C) 2014-2015 Gary Wallis for Unixservice, LLC.
+
+cEncAlg="arcfour";
+#
+#user conf section
+#required local settings. mysql and iptables
+if [ -f "/etc/unxsvz/mysql.local.sh" ];then
+	source /etc/unxsvz/mysql.local.sh;
+else
+	echo "no /etc/unxsvz/mysql.local.sh";
+	exit 1;
+fi
+if [ -f "/etc/unxsvz/scp.local.sh" ];then
+	source /etc/unxsvz/scp.local.sh;
+fi
+#
+#
+
+if [ ! $1 ] || [ ! -f $1 ]; then
+        echo "usage $0: <fully qualified path of file to copy> [--remotedatacenter]";
+        exit 0;
+fi
+
+cShortHostname=`hostname -s`;
+if [ $? != 0 ];then
+	echo "hostname -s failed";
+	exit 1;
+fi
+uNode=`$cMySQLConnect -N -e "SELECT uNode FROM tNode WHERE cLabel='$cShortHostname' AND uStatus=1"`;
+if [ $? != 0 ];then
+	echo "Select node failed $cShortHostname";
+	exit 1;
+fi
+if [ "$uNode" == "" ];then
+	echo "uNode failed";
+	exit 1;
+fi
+uDatacenter=`$cMySQLConnect -N -e "SELECT uDatacenter FROM tNode WHERE uNode=$uNode"`;
+if [ $? != 0 ];then
+	echo "Select datacenter failed $uNode";
+	exit 1;
+fi
+if [ "$uDatacenter" == "" ];then
+	echo "uDatacenter failed";
+	exit 1;
+fi
+cServerList=`$cMySQLConnect -N -e "SELECT cLabel FROM tNode WHERE uStatus=1"`;
+if [ $? != 0 ];then
+	echo "Select cLabel failed $uDatacenter";
+	exit 1;
+fi
+
+#echo $cServerList;
+#exit;
+
+for cServer in $cServerList; do
+        if [ "$cServer" != "$cShortHostname" ];then
+                echo $cServer;
+                /usr/bin/scp -c $cEncAlg "$1" $cServer:"$1";
+        fi
+done
+
+exit 0;
