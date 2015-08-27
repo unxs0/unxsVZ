b75896876119c54d75b17aebb0a61d8292295fcf
Colin <Colin@7028d9a3-dfe6-4776-a87e-dad94dd282e0>
2015-08-27 20:36:43 +0000

    Set status of a given veid.

---
+++ trunk/unxsScripts/unxsvz/sbin/unxsvzSetContainerStatus.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#FILE
+#       /usr/sbin/unxsvzSetContainerStatus.sh 
+#PURPOSE
+#       Change container status 
+#LEGAL
+#	Copyright (C) Ricardo Armas for Unixservice, LLC.
+#	GPLv2 license applies.
+
+if [ "$1" == "" ] || [ "$2" == "" ];then
+        echo "usage: $0 <uVEID> <uStatus>";
+        exit 0;
+fi 
+
+cMySQLConnect="";
+if [ -f "/etc/unxsvz/mysql.local.sh" ];then
+	source /etc/unxsvz/mysql.local.sh;
+else
+	echo "no /etc/unxsvz/mysql.local.sh";
+	exit 1;
+fi
+if [ -f "/etc/unxsvz/unxsvz.functions.sh" ];then
+	source /etc/unxsvz/unxsvz.functions.sh;
+else
+	echo "no /etc/unxsvz/unxsvz.functions.sh";
+	exit 1;
+fi
+if [ "$cMySQLConnect" == "" ];then
+	fLog "no cMySQLConnect";
+	exit 1;
+fi
+
+uContainer=`echo "SELECT uContainer FROM tContainer WHERE uContainer=$1"|$cMySQLConnect -B -N`;
+if [ $? != 0 ] || [ "$uContainer" == "" ];then
+	fLog "uVEID SELECT error";
+	exit 1;
+fi
+
+uStatus=`echo "SELECT uStatus FROM tStatus WHERE uStatus=$2"|$cMySQLConnect -B -N`;
+if [ $? != 0 ] || [ "$uStatus" == "" ];then
+        fLog "uStatus SELECT error";
+        exit 1;
+fi
+
+echo "UPDATE tContainer SET uStatus=$uStatus WHERE uContainer=$uContainer"|$cMySQLConnect;
+
+
