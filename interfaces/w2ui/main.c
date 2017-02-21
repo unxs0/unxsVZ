d4d90e40d5ac00d7dc0944ed2effc48a2e653d4e
root <root@unixservice.com>
2017-02-21 04:43:59 -0800

    First basic working json server.

--- interfaces/w2ui/main.c
+++ interfaces/w2ui/main.c
@@ -38,13 +38,43 @@ int main(int argc, char *argv[])
         MYSQL_RES *res;
 	MYSQL_ROW field;
 
+/*
+{
+    "status"  : "success",
+    "total"   : 36,
+    "records" : [
+        { "recid": 1, "field-1": "value-1", ... "field-N": "value-N" },
+        ...
+        { "recid": N, "field-1": "value-1", ... "field-N": "value-N" }
+    ]
+}
+*/
+
+	printf("Content-type: text/json\n\n");
 	sprintf(gcQuery,"SELECT NOW();");
 	mysql_query(&gMysql,gcQuery);
 	res=mysql_store_result(&gMysql);
-	while((field=mysql_fetch_row(res)))
+	unsigned uNumRows=0;
+	printf("{\n");
+	if((uNumRows=mysql_num_rows(res))>0)
 	{
-		printf("%s\n",field[0]);
+		printf("    \"status\" : \"success\",\n");
+		printf("    \"total\" : %u,\n",uNumRows);
+		printf("    \"records\" : [\n");
+
+		while((field=mysql_fetch_row(res)))
+		{
+			printf("{ \"recid\": 1, \"fname\": \"%s\" }\n",field[0]);
+		}
+		printf("    ]\n");
+
 	}
+	else
+	{
+		printf("    \"status\" : \"failure\",\n");
+	}
+	printf("}\n");
+
 	mysql_free_result(res);
 	return(0);
 
