fd78570086164689c280118d3d8a00a30ff7d75c
root <root@unxs9.unixservice.com>
2020-07-03 18:12:03 -0500

    Slowly adding flesh

--- unxsBind/tools/export/idns-exportXMLzones.c
+++ unxsBind/tools/export/idns-exportXMLzones.c
@@ -6,14 +6,10 @@ PURPOSE
 STANDARDS
 	https://tools.ietf.org/id/draft-daley-dnsxml-00.html
 ISSUES
-	1-. Customer provides example with non standard zone, and record dnsxml rr types.
-	Do we follow as extension, as per standard above? Or ignore using
-	3.2.1. Base RR element and base attributes. E.g. owner is the zone with subzones?
-	2-. The current code, proof of concept based on email from D. Rogal, does not adhere to the
-	dnsxml standard as set forth in ietf draft.
 WORK IN PROGRESS
 	SOA record at new tZone is required.
-	Remove non standard zone and record XML elements.
+	NS records must come from tNSSet mess.
+	Also check for main address and special MX sets. These come from tZone.
 LEGAL
 	(C) Gary Wallis 2001-2020. All Rights Reserved.
 	LICENSE file should be included in distribution.
@@ -48,10 +44,15 @@ int main(int iArgc, char *cArg[])
 			"\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
 			"\txsi:schemaLocation=\"urn:ietf:params:xml:ns:dns dnsxml-1.0.xsd\">\n");
 
-	sprintf(gcQuery,"SELECT tZone.cZone,tZone.uZone,tView.cLabel"
-			" FROM tZone,tView"
+	sprintf(gcQuery,"SELECT tZone.cZone,tZone.uZone,tView.cLabel,"
+			"tNS.cFQDN,tZone.cHostmaster,tZone.uSerial,"
+			"tZone.uRefresh,tZone.uRetry,tZone.uExpire,tZone.uTTL,"
+			"tZone.cMainAddress,tZone.uMailServers"
+			" FROM tZone,tView,tNS"
 			" WHERE tZone.uView=tView.uView"
-			" AND tZone.uView=2 AND tZone.cZone LIKE '%s'",cArg[1]);
+			" AND tZone.uNSSet=tNS.uNSSet"
+			" AND tNS.uNSType=1"
+			" AND tZone.uView=2 AND tZone.cZone LIKE '%s' ORDER BY tNS.uNS",cArg[1]);
 	mysql_query(&gMysql,gcQuery);
 	if(mysql_errno(&gMysql)) 
 	{
@@ -61,7 +62,16 @@ int main(int iArgc, char *cArg[])
 	res=mysql_store_result(&gMysql);
 	while((field=mysql_fetch_row(res)))
 	{
-		printf("\t<zone=\"%.99s\">\n",field[0]);
+		printf("\t<RRSet owner=\"%.99s\" class=\"IN\" ttl=\"0\">\n",field[0]);
+
+
+		//tZone specified records
+		printf("\t\t<SOA mname=\"%s\" rname=\"%s\" serial=\"%s\" refresh=\"%s\" retry=\"%s\" expire=\"%s\" minimum=\"%s\"/>\n",
+				field[3],field[4],field[5],field[6],field[7],field[8],field[9]);
+		//TODO one or more like so...
+		printf("\t\t<NS owner=\"\" class=\"IN\" ttl=\"0\" address=\"1.2.3.4\/>");
+		//if cMainAddress add A record
+		//if uMailServer add associated MX records
 
 		sprintf(gcQuery,"SELECT tRRType.cLabel,tResource.cName,tResource.uTTL,tResource.cParam1,tResource.cParam2"
 			" FROM tResource,tRRType"
@@ -75,69 +85,70 @@ int main(int iArgc, char *cArg[])
 		res2=mysql_store_result(&gMysql);
 		while((field2=mysql_fetch_row(res2)))
 		{
+
+			if(!strcmp(field2[1],"@")) field2[1][0]=0;
 			if(!strcmp(field2[0],"A"))
 			{
 				//<record type="A" owner="mail.abc.com." class="IN" ttl="86400" address="1.2.3.5"/>
-				printf("\t\t<record type=\"A\" owner=\"%s\" class=\"IN\" ttl=\"%s\" address=\"%s\"/>\n",
+				printf("\t\t<A owner=\"%s\" class=\"IN\" ttl=\"%s\" address=\"%s\"/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"AAAA"))
 			{
 				//<record type="AAAA" owner="mail.abc.com." class="IN" ttl="86400" ip6address="::1::1234::"/>
-				printf("\t\t<record type=\"AAAA\" owner=\"%s\" class=\"IN\" ttl=\"%s\" ip6address=\"%s\"/>\n",
+				printf("\t\t<AAAA owner=\"%s\" class=\"IN\" ttl=\"%s\" ip6address=\"%s\"/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"CNAME"))
 			{
 				//<record type="CNAME" owner="mail.abc.com." class="IN" ttl="86400" host="blabla.com"/>
-				printf("\t\t<record type=\"CNAME\" owner=\"%s\" class=\"IN\" ttl=\"%s\" host=\"%s\"/>\n",
+				printf("\t\t<CNAME owner=\"%s\" class=\"IN\" ttl=\"%s\" host=\"%s\"/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"HINFO"))
 			{
-				printf("\t\t<record type=\"HINFO\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<HINFO owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"MX"))
 			{
-				printf("\t\t<record type=\"MX\" owner=\"%s\" class=\"IN\" ttl=\"%s\" preference=\"%s\""
+				printf("\t\t<MX owner=\"%s\" class=\"IN\" ttl=\"%s\" preference=\"%s\""
 						" exchange=\"%s\"/>\n",
 					field2[1],field2[2],field2[3],field2[4]);
 			}
 			else if(!strcmp(field2[0],"NAPTR"))
 			{
-				printf("\t\t<record type=\"NAPTR\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<NAPTR owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"NS"))
 			{
-				printf("\t\t<record type=\"NS\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<NS owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"PTR"))
 			{
-				printf("\t\t<record type=\"PTR\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<PTR owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"SRV"))
 			{
-				printf("\t\t<record type=\"SRV\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<SRV owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[1],field2[2],field2[3]);
 			}
 			else if(!strcmp(field2[0],"TXT"))
 			{
-				printf("\t\t<record type=\"TXT\" owner=\"%s\" class=\"IN\" ttl=\"%s\">\n",
-					field2[1],field2[2]);
-				printf("\t\t<TXT>%s</TXT>\n\t\t</record>\n",field2[3]);
+				printf("\t\t<TXT owner=\"%s\" class=\"IN\" ttl=\"%s\">%s</TXT>\n",
+					field2[1],field2[2],field2[3]);
 			}
 			else if(1)
 			{
-				printf("\t\t<record type=\"%s\" owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
+				printf("\t\t<%s owner=\"%s\" class=\"IN\" ttl=\"%s\" NOT-IMPLEMENTED/>\n",
 					field2[0],field2[1],field2[2]);
 			}
 		}
 
-		printf("\t</zone>\n");
+		printf("\t</RRSet>\n");
 	}
 	printf("</dnsxmls>\n");
 
